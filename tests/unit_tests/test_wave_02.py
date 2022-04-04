3c53be89e668353ea03e5d2003546913bc568301
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-04-04 14:01:08 -0500

    Updated test_get_no_matching_items_by_category to be more comprehensive

--- tests/unit_tests/test_wave_02.py
+++ tests/unit_tests/test_wave_02.py
@@ -31,7 +31,10 @@ def test_get_no_matching_items_by_category():
 
     items = vendor.get_by_category("electronics")
 
-    assert items == []
+    assert len(items) == 0
+    assert item_a not in items
+    assert item_b not in items
+    assert item_c not in items
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
