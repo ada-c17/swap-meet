1eb14a1f25e5f923656c1e076cd2fbdac669efc2
Grace <gpark439@gmail.com>
2022-04-04 16:26:09 -0700

    Define Clothing class with attribute category, pass test_wave_05 test #1.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,8 @@
 class Clothing:
-    pass
+    
+    def __init__(self):
+        self.category = "Clothing"
+
+    # stringify method for Clothing
+    def __str__(self):
+        return "The finest clothing you could wear."
