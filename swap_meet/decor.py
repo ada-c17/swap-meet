9bd76c5b87461bc543dab9527b31252462f8f07f
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 15:11:28 -0700

    add logic to Decor class as per instructions in Wave 5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,11 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+class Decor(Item):
+
+    category = "Decor"
+
+    def __init__(self, condition=0):
+        super().__init__(Decor.category, condition)
+
+    @staticmethod
+    def __str__():
+        return "Something to decorate your space."
