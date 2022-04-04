385122f7790ec92a1a66234a6c0e3ee838462a27
Shayla L <88570826+compugrl@users.noreply.github.com>
2022-04-04 15:45:17 -0700

    added functionality for wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category = "Electronics", condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,21 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        cond_dict = {
+            0: "OMG, that's really bad!!",
+            1: "It can only go up from here",
+            2: "You might want to work on this one a bit", 
+            3: "Just average",
+            4: "Pretty darn good!",
+            5: "Woah!! This is stellar!"
+            }
+
+        verbiage = cond_dict[self.condition]
+        return verbiage
+        
