c09666a4da0259282c6bf418244332617bf3dcf6
Gabriela Webb <gabw813@gmail.com>
2022-04-04 11:54:02 -0700

    defined vendor class; passed wave 1 test 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,10 @@
 class Vendor:
-    pass
     # wave 1
-    
     # attributes: 
     #   inventory - an empty list
     # instance methods:
     #   add - takes in one item and adds to inventory, returns item added
     #   remove - takes in one item and removes from inventory, returns item removed
+
+    def __init__(self):
+        self.inventory = []
