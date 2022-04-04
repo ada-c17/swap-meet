9edb2d06651f57e84f00d74de8a5f2293aeee775
sarah <skiyoko14@gmail.com>
2022-04-04 11:36:16 -0700

    define vendor class, pass wave01 tests except self-written test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        try:
+            self.inventory.remove(item)
+        except ValueError:
+            return False
+        return item
