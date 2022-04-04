9f5f7e663377a9984e95d8398e4943b37ee5263a
Natascha <njenkns@gmail.com>
2022-04-04 11:53:11 -0700

    passed unit tests in wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,13 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]): 
+        self.inventory = inventory
+    
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item 
+    
+    def remove(self, rem_item):
+        if rem_item in self.inventory:
+            self.inventory.remove(rem_item)
+            return rem_item
+        return False 
