e0065b533c11d4f44f1c7d064f2ccf9aaeabe4ab
Daria <daria.iakymenko@gmail.com>
2022-04-04 13:48:24 -0700

    completed Vendor class, passed tests for Wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
+from operator import inv
+
+
 class Vendor:
-    pass
+    def __init__(self, inventory= None):
+        if not inventory:
+            inventory = []
+        self.inventory = inventory
+        
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+        
+
