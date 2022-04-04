6c794e15461e0296df564d6fd5db29173270d414
Kiana <kianashui@gmail.com>
2022-04-04 13:51:04 -0700

    added Item inheritance for Clothing, Decor, and Electronics

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,21 +1,8 @@
-class Clothing:
+from swap_meet.item import Item
+
+class Clothing(Item):
     def __init__(self, condition=0):
         self.category = "Clothing"
         self.condition = condition
     def __str__(self):
         return "The finest clothing you could wear."
-    
-    def condition_description(self):
-        if self.condition == 0:
-            condition_desc = "pls no"
-        elif self.condition == 1:
-            condition_desc = "heavily used"
-        elif self.condition == 2:
-            condition_desc = "fair"
-        elif self.condition == 3:
-            condition_desc = "very good"
-        elif self.condition == 4:
-            condition_desc = "like new"
-        elif self.condition == 5:
-            condition_desc = "mint"
-        return condition_desc
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,20 +1,8 @@
-class Electronics:
+from swap_meet.item import Item
+
+class Electronics(Item):
     def __init__(self, condition=0):
         self.category = "Electronics"
         self.condition = condition
     def __str__(self):
         return "A gadget full of buttons and secrets."
-    def condition_description(self):
-        if self.condition == 0:
-            condition_desc = "pls no"
-        elif self.condition == 1:
-            condition_desc = "heavily used"
-        elif self.condition == 2:
-            condition_desc = "fair"
-        elif self.condition == 3:
-            condition_desc = "very good"
-        elif self.condition == 4:
-            condition_desc = "like new"
-        elif self.condition == 5:
-            condition_desc = "mint"
-        return condition_desc
