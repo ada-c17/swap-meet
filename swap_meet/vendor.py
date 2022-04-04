aec0ff02b47f501561846a2e9ff28f619ce2400d
verasazonova <vera.zhokhova@gmail.com>
2022-04-04 14:18:52 -0700

    Add comments to Vendor file

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,13 +1,16 @@
 class Vendor:
+    # create inventory parameters, where deafalt is empty list
     def __init__(self,inventory = None):
         if inventory is None:
             inventory = []
         self.inventory = inventory
     
+    # create instance method add which add things in inventory list
     def add(self,item):
         self.inventory.append(item)
         return item
 
+    # create instance method remove which remove item from inventory list
     def remove(self,item):
         for items in self.inventory:
             if item == items:
