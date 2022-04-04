3b75b931e54cbeab0b75165245ff79f59113c3bc
NinaPatrina <ninochca@gmail.com>
2022-04-04 11:40:37 -0700

    Created Vendor class. Passed tests wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    def __init__(self, inventory =[]):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            indx=self.inventory.index(item)
+            item_removed=self.inventory.pop(indx)   
+        else:
+            return False
+        return item_removed    
+
