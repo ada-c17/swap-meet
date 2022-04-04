cd9c472e9d1f21ff1b2c17dcdfbeaaf6320ba91a
Katherine Otten Kim <kkatherine317@gmail.com>
2022-04-04 11:47:01 -0700

    test passes for wave1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if not item in self.inventory:
+            return False 
+        self.inventory.remove(item)
+        return item
+    
+
+
