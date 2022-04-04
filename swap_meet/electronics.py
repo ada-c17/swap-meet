72e85dbe28046c1656ed3184fa489eaf828659dd
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 15:13:54 -0700

    add logic to Electronics class as per instructions in Wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+class Electronics(Item):
+
+    category = "Electronics"
+
+    def __init__(self, condition=0):
+        super().__init__(Electronics.category, condition)     
+
+    @staticmethod
+    def __str__():
+        return "A gadget full of buttons and secrets."
