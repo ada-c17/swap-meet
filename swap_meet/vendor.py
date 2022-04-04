cfcb4909d335296383b16b5059985f0e76182e65
verasazonova <vera.zhokhova@gmail.com>
2022-04-04 14:10:59 -0700

    Resolve wave 1 and 1st test wave

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    def __init__(self,inventory = None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        for items in self.inventory:
+            if item == items:
+                self.inventory.remove(items)
+                return items
+        return False
