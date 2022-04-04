121a9383573a8f3fd68eb80750dbb7f144d585bd
Roshni Patel <rpatel01@wesleyan.edu>
2022-04-04 11:30:18 -0700

    Wave 2 tests passing

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,5 @@
+from swap_meet import vendor
+
 class Item:
     def __init__(self, category = ""):
         self.category = category 
-
-    def get_by_category(self, category):
-        pass 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,4 +13,15 @@ class Vendor:
         self.inventory.remove(item)
         return item 
 
+    def get_by_category(self, category):
+        categorized_list = []
+        for item in self.inventory:
+            if item.category == category:
+                categorized_list.append(item)
 
+        if len(categorized_list) >= 1:
+            return categorized_list
+        else: 
+            return None 
+
+        # categorized_list if len(categorized_list) >= 1 else None 
