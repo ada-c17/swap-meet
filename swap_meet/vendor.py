a4afa4442a6d6a6a24e06b272d9b0c55a6f1b2c7
Christina Webber <chrisswebb@yahoo.com>
2022-04-04 13:01:45 -0500

    vendor.py completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in set(self.inventory):
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
