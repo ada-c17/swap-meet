3578a3c381e08ff0358d34ccb3b2d17243e18a26
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-04-04 12:13:07 -0700

    Created class Item, ran tests wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,7 @@
 class Item:
-    pass
+    def __init__(self, category = ""):
+        '''
+        Constructs attributes for Item object, default category value is empty string.
+        '''
+        self.category = category
+
