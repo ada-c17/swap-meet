be7e9412561f975f0ac4f8831fda83fc1b6f9c5d
sarah <skiyoko14@gmail.com>
2022-04-04 12:04:43 -0700

    pass wave03 tests with swap_items function defined

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,3 +20,16 @@ class Vendor:
             if item.category == category:
                 items_in_category.append(item)
         return items_in_category
+
+    def swap_items(self, vendor, my_item, their_item):
+        #this if calls .remove regardless of outcome;
+        #will cause item removal if present in inventory
+        if not (vendor.remove(their_item)):
+            return False
+        if not (self.remove(my_item)):
+            vendor.add(their_item)
+            return False
+
+        vendor.add(my_item)
+        self.add(their_item)
+        return True
