4ed6536644193fdd51312aa809732e42436015d4
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-04 13:20:59 -0700

    Implement code for Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,25 @@
+#from swap_meet.item import Item
+
 class Vendor:
-    pass
+
+    def __init__(self, inventory = None):
+        # why do we specify that it's a list as a default param?
+        if not inventory:
+           inventory = []
+        self.inventory = inventory
+
+    def add(self, item):
+        if not item:
+            return False
+
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        #inventory_copy = self.inventory.deepcopy
+        if item not in self.inventory:
+            return False
+
+        self.inventory.remove(item)
+        return item
+
