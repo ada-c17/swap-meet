9a530bc01e6134f2bdfc489d2ddabf6956aa6111
Thu Vuong <tavuongg@gmail.com>
2022-04-04 12:10:27 -0700

    completed wave 2, assertions, and passed test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,9 @@
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = []
     
     def add(self, item):
         self.inventory.append(item)
@@ -11,4 +14,17 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+
+        # refactor
+        # try:
+        #     self.inventory.remove(item)
+        #     return item
+        # except ValueError:
+        #     return False
+
+    def get_by_category(self, category):
+
+        # utilized list comprehension
+        category_items = [item for item in self.inventory if item.category == category]
+        return category_items
 
