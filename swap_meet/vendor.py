0e3b752fe20ab647b5f77a457cdcac9bc2925695
Nikki Torab <nikkitorab@gmail.com>
2022-04-04 11:36:41 -0700

    wave 1: 4/5 tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    # This method adds the item to the inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    # This method removes the matching item from the inventory
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
