552824e84708df4151ed6e0a9d67910d0eee87d4
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-04-04 13:38:14 -0500

    complete wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,46 @@
 class Vendor:
-    pass
+    """
+    A class to represent a vendor.
+
+    Attributes
+    ----------
+    inventory : list
+        list of items in inventory
+        optional attribute defaulted aas None 
+        Uses logic to create empty dictionary if needed
+
+    Methods
+    -------
+    add(item):
+        appends new item to inventory list
+    remove(item):
+        removes item from inventory list
+        if item not in list, raises ValueError and returns False
+    """
+
+    def __init__(self, inventory = None):
+        """
+        Constructs all the necessary attributes for the person object.
+
+        Parameters
+        ----------
+            inventory : list
+                list of vendor items
+        """
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        """Takes in an item and appends to inventory list."""
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        """Removes specific item from inventory list if available, if not, returns False."""
+        try:
+            self.inventory.remove(item)
+            return item
+        except ValueError:
+            return False
