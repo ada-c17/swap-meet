72fd5842d29cd0d58d3052dd90a7452ff62b9627
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-04-04 14:36:47 -0400

    passes Wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+    
+    def add(self,*items):
+        if not items:
+            return None
+        for item in items:
+            self.inventory.append(item)
+        return items[0] if len(items) == 1 else items
+    
+    def remove(self,*items):
+        if not items:
+            return None
+        for item in items:
+            if item not in self.inventory:
+                return False
+            self.inventory.remove(item)
+        return items[0] if len(items) == 1 else items
