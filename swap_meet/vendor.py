fa7beec331e0ff9cb2211c0f177d9c61c42efbf2
Jodi Denney <iarrived@gmail.com>
2022-04-04 13:55:58 -0700

    Passed Wave 1 and Wave 2 unit tests.  Creates Vendor and Item Classes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        return [item for item in self.inventory if item.category == category]
