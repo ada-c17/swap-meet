73e5a5efea59d28f74b8b05f8a638cf38767e8a3
Christina Webber <chrisswebb@yahoo.com>
2022-04-04 13:09:11 -0500

    unit tests wave 2 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,3 +13,10 @@ class Vendor:
             return item
         else:
             return False
+    
+    def get_by_category(self, category):
+        cat_list = []
+        for i in self.inventory:
+            if i.category == category:
+                cat_list.append(i)
+        return cat_list
