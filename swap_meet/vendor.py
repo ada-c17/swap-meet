d254c0dae2b2702cc3f41ab3cd6f70ea7c9e645d
Kiana <kianashui@gmail.com>
2022-04-04 11:53:32 -0700

    added wave 2 functionality and passing wave 1 and 2 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,12 +1,22 @@
 class Vendor:
     def __init__(self, inventory=[]):
         self.inventory = inventory
+
     def add(self, item):
         self.inventory.append(item)
         return item
+
     def remove(self, item):
         try:
             self.inventory.remove(item)
             return item
         except ValueError:
             return False
+
+    def get_by_category(self, category):
+        category_items = []
+        for item in self.inventory:
+            if item.category == category:
+                category_items.append(item)
+
+        return category_items
