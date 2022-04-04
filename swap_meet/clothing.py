bceaace139260e39e6268fda604ce9869bd56983
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 15:10:12 -0700

    add logic to Clothing class as per instructions in Wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from unicodedata import category
+from swap_meet.item import Item
+class Clothing(Item):
+    
+    category = "Clothing"
+    
+    def __init__(self, condition=0):
+        super().__init__(Clothing.category, condition)
+
+    @staticmethod
+    def __str__():
+        return "The finest clothing you could wear."
