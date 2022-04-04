57a3bec224d095e0c9bff8acded49132ffd91d6e
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-04-04 12:01:37 -0700

    Added docstrings to Class Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,25 @@
 class Vendor:
     #insert doc strings
+
     def __init__(self, inventory = None):
+        '''
+        Constructs attributes for Vendor object, default inventory value is empty list.
+        '''
         if not inventory:
             inventory = []
         self.inventory = inventory
 
     def add(self, item):
+        '''
+        Adds item to attribute inventory, returns item.
+        '''
         self.inventory.append(item)
         return item
     
     def remove(self, item):
+        '''
+        Removes item from attribute inventory if item in list, returns item; else returns False.
+        ''' 
         if item in self.inventory:
             self.inventory.remove(item)
             return item
