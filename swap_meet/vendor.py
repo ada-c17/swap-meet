86c2485c3d962d50b54bd23a04d1c9d0fc77fca4
Jenny <jfperham@gmail.com>
2022-04-04 12:10:31 -0700

    finished wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,15 @@
 class Vendor:
-    pass
-# testing out adding to git
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, item_to_remove):
+        if item_to_remove in self.inventory:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
+        else:
+            return False
+    
