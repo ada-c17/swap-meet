53445cbb0b04b09168e52c71876913165a89e630
Amel <amelntamark@gmail.com>
2022-04-04 11:49:27 -0700

    implement instance method for vendor called remove

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -5,3 +5,7 @@ class Vendor:
     def add(self, item):
         self.inventory.append(item)
         return item
+
+    def remove(self, item):
+        self.inventory.remove(item)
+        return item
