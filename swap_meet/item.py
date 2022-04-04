8af6cc3df697ad7b67f5364025d0c9e8d8e45d22
Uyen Vong <uvong92@gmail.com>
2022-04-04 14:48:13 -0700

    pass wave 5 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,26 @@
 class Item:
-    def __init__(self, category=None):
+    def __init__(self, category=None, condition=0):
         if category is None:
             self.category = ""
         else:
             self.category = category
+            self.condition = condition
     
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 5:
+            return "Like new"
+        elif self.condition == 4:
+            return "Very good"
+        elif self.condition == 3:
+            return "Good"
+        elif self.condition == 2:
+            return "Acceptable"
+        elif self.condition == 1:
+            return "Ugly"
+        elif self.condition == 0:
+            return "As is"
     
 
