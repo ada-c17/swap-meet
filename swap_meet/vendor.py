795eda248990168170703bc6391c1abfd2bc87ab
Andrea Garcia <andrygzt@gmail.com>
2022-04-04 13:27:17 -0700

    The one when the first wave passes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory =[]):
+        self.inventory = inventory 
+
+    def add(self, item_added):
+        new_item = self.inventory.append(item_added)
+        return item_added
+
+    def remove(self, item_removed):
+        if item_removed in self.inventory:
+            self.inventory.remove(item_removed)
+            return item_removed
+        else:
+            return False
+        
+
+
