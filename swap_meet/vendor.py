449cdc0000ed5b9ee1fd50e9e7b9436ac16986b0
Sophia Wagner <sophiewagnerla@gmail.com>
2022-04-04 11:58:38 -0700

    Passed W1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
-class Vendor:
-    pass
+class Vendor:   
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        try:
+            self.inventory.remove(item)
+            return item
+        except ValueError:
+            return False
