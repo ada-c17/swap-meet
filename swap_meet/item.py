e69376b79d83bab43c4d1763fc73630e3178f775
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-04-04 15:14:22 -0500

    complete wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -13,10 +13,25 @@ class Item:
         overrides str() method to return "Hello World!"
     """
     
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0.0):
         """ Constructs all the necessary attributes for the item object. """
         self.category = category
+        self.condition = condition
     
     def __str__(self):
         """ Overrides string method. Returns "Hello World!" """
         return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "This is a hot mess. Run!"
+        elif self.condition == 1:
+            return "Barely worth the money."
+        elif self.condition == 2:
+            return "Somewhat decent."
+        elif self.condition == 3:
+            return "Not too shabby"
+        elif self.condition == 4:
+            return "This is some good stuff!"
+        elif self.condition == 5:
+            return "Beyonce herself would want this!"
