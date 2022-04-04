d16ef749d30620ae40e09614713c8f1befd51dbd
Ada <adabarries@gmail.com>
2022-04-04 11:48:48 -0700

    class Vendor defined, wave_01 unit tests passed.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        """
+        takes in one item
+        adds item to inventory
+        returns item that was added
+        """
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False            
