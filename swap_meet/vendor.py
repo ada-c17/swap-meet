abbdd2772cdd1fcec58cc6284e13305752f7a348
Gabriela Webb <gabw813@gmail.com>
2022-04-04 11:45:49 -0700

    test commit

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,9 @@
 class Vendor:
     pass
+    # wave 1
+    
+    # attributes: 
+    #   inventory - an empty list
+    # instance methods:
+    #   add - takes in one item and adds to inventory, returns item added
+    #   remove - takes in one item and removes from inventory, returns item removed
