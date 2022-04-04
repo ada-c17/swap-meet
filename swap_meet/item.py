58dfa0c6ac2ccb516f7c81207ae2dc6fbe6ed992
Grace <gpark439@gmail.com>
2022-04-04 16:05:31 -0700

    Add method to stringify Item, add method to swap items from vendor to other vendor's inventory. Pass test_wave_03 all tests.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,9 @@
 class Item:
     
+    # constructor for class Item
     def __init__(self, category = ""):
         self.category = category
+
+    # method to stringify an Item
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,13 +1,17 @@
 from swap_meet.item import Item
 
 class Vendor:
+    
+    # constructor for class Vendor
     def __init__(self, inventory = []):
         self.inventory = inventory
 
+    # method to add item to inventory
     def add(self, item):
         self.inventory.append(item)
         return item
     
+    # method to remove item from inventory
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
@@ -15,6 +19,7 @@ class Vendor:
         else:
             return False
 
+    # method to return list of items by item category
     def get_by_category(self, category):
         item = Item()
         items = []
@@ -22,3 +27,13 @@ class Vendor:
             if item.category == category:
                 items.append(item)
         return items
+
+    # method to swap from vendor's inventory to friend's inventory
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.remove(my_item)
+            self.add(their_item)
+            other_vendor.remove(their_item)
+            other_vendor.add(my_item)
+            return True
+        return False
