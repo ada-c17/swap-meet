17400d97bd582d23dbaebefbcfb679692eb1124c
Shayla L <88570826+compugrl@users.noreply.github.com>
2022-04-04 14:13:55 -0700

    coded functionality for waves 1-3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,37 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        self.category = category
+        item_list = []
+        for cat in self.inventory:
+            if cat.category == self.category:
+                item_list.append(cat)
+
+        return item_list
+
+    def swap_items(self, vendor2, my_item, their_item):
+        self.vendor2 = vendor2
+        self.my_item = my_item
+        self.their_item = their_item
+
+        if self.my_item not in self.inventory or their_item not in vendor2.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            vendor2.inventory.append(my_item)
+            vendor2.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True
