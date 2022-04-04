6ed854bffcd3c9d63691f810a3d4d3729c580ca6
Christina Webber <chrisswebb@yahoo.com>
2022-04-04 13:43:38 -0500

    unit tests wave 3 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,3 +20,18 @@ class Vendor:
             if i.category == category:
                 cat_list.append(i)
         return cat_list
+    
+    def swap_items(self, other, my_item, their_item):
+        if my_item in self.inventory:
+            self.remove(my_item)
+            other.add(my_item)
+        else:
+            return False
+        if their_item in other.inventory:
+            other.remove(their_item)
+            self.add(their_item)
+        else:
+            self.add(my_item)
+            other.remove(my_item)
+            return False
+        return True
