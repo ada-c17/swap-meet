2f9254e24172a4b07cb2cf4834ed1746bd899422
Katherine Otten Kim <kkatherine317@gmail.com>
2022-04-04 13:35:49 -0700

    passes tests in wave5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,21 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
 
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        rounded_number = round(self.condition)
+        if rounded_number == 0:
+            return "Heavily used!"
+        elif rounded_number == 1:
+            return "Moderately used!"
+        elif rounded_number == 2:
+            return "Slightly used!"
+        elif rounded_number == 3:
+            return "Used but well maintained!"
+        elif rounded_number == 4:
+            return "Just like brand new!"
+        elif rounded_number == 5:
+            return "Brand new!"
