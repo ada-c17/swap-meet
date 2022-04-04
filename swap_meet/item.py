cf8f020cb6c0fdc09d286b76328b10609fe89672
Jenny <jfperham@gmail.com>
2022-04-04 16:08:27 -0700

    passed wave 2, working on wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,9 @@
 class Item:
-    pass
+    def __init__(self, category=""):
+        self.category = category
+    #stringify item
+    def __str__(self):
+        return "Hello World!"
+
+# When we initialize an instance of Item,
+# we can optionally pass in a string with the keyword argument category
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,4 +12,20 @@ class Vendor:
             return item_to_remove
         else:
             return False
-    
+
+    def get_by_category(self, category):
+        ''' input: string, representing a category. Output: list of Items in the inventory w/that category'''
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+            if items == []:
+                return False
+        return items
+        
+    # def swap_items(self, Vendor(friends_inventory), Item(my_item), Item(their_item)):
+        pass
+#     It removes the my_item from this Vendor's inventory, and adds it to the friend's inventory
+# It removes the their_item from the other Vendor's inventory, and adds it to this Vendor's inventory
+# It returns True
+# If this Vendor's inventory doesn't contain my_item or the friend's inventory doesn't contain their_item, the method returns False
