ebee7b9d125a97e7d2667617b89d0b431fe571f2
Natascha <njenkns@gmail.com>
2022-04-04 11:56:56 -0700

    added docstring to vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,6 @@
 class Vendor:
+    '''Creating Vendor class that has an inventory attribute which is an optional argument. The add instance method adds a new item to inventory and the remove instance method
+    removes an item if that item is found in the inventory list. If item is not found in list to remove, False is returned. '''
     def __init__(self, inventory=[]): 
         self.inventory = inventory
     
