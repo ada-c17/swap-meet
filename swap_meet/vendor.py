f88f697c0aa362d28620b7049622d1e9567d5977
Hillary Smith <hillaryhsmith@gmail.com>
2022-04-04 11:56:04 -0700

    passed waves 1 and 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
+
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        try: 
+            self.inventory.remove(item)
+            return item
+        except ValueError as error:
+            return False
+    
+    def get_by_category(self, category):
+        items_in_category = []
+        for item in self.inventory: 
+            if item.category == category:
+                items_in_category.append(item)
+        return items_in_category
