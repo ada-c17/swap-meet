e3494d4ad16809af6e913e4eb1fe24f1aac399b0
Elaine Smith <oneile1219@gmail.com>
2022-04-04 12:12:17 -0700

    Wave 01 - completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
+
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+        
