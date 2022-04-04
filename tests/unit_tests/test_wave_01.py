6a89599ce72af03789cfe59473a3b6793fa53d26
Lin Liu <lin_6066@yahoo.com>
2022-04-04 12:00:13 -0700

    pass unit test wave 1

--- tests/unit_tests/test_wave_01.py
+++ tests/unit_tests/test_wave_01.py
@@ -51,6 +51,7 @@ def test_removing_not_found_is_false():
 
     # raise Exception("Complete this test according to comments below.")
     assert result == False
+    assert len(vendor.inventory) == 3
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
