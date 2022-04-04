997aaea4e2544d8aecb31781f03061ccea231b9d
Joanna Dudley <jfdudleybmc@gmail.com>
2022-04-04 14:08:45 -0400

    completed creation of Vendor class, passing all wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+    
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, discarded_item):
+        if discarded_item not in self.inventory:
+            return False
+        else: 
+            self.inventory.remove(discarded_item)
+            return discarded_item
