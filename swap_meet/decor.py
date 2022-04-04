f4ac2eca27b49f9ecfc96a70cf6a5033ecf895d7
Kiana <kianashui@gmail.com>
2022-04-04 13:44:28 -0700

    added wave 5 functionality and passing tests for waves 1-5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,21 @@
 class Decor:
-    pass
+    def __init__(self, condition=0):
+        self.category = "Decor"
+        self.condition = condition
+    def __str__(self):
+        return "Something to decorate your space."
+    
+    def condition_description(self):
+        if self.condition == 0:
+            condition_desc = "pls no"
+        elif self.condition == 1:
+            condition_desc = "heavily used"
+        elif self.condition == 2:
+            condition_desc = "fair"
+        elif self.condition == 3:
+            condition_desc = "very good"
+        elif self.condition == 4:
+            condition_desc = "like new"
+        elif self.condition == 5:
+            condition_desc = "mint"
+        return condition_desc
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,20 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
     def __str__(self):
         return "Hello World!"
+    def condition_description(self):
+        if self.condition == 0:
+            condition_desc = "pls no"
+        elif self.condition == 1:
+            condition_desc = "heavily used"
+        elif self.condition == 2:
+            condition_desc = "fair"
+        elif self.condition == 3:
+            condition_desc = "very good"
+        elif self.condition == 4:
+            condition_desc = "like new"
+        elif self.condition == 5:
+            condition_desc = "mint"
+        return condition_desc
