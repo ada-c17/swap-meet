e2ded35e3ef1d3546c61f857f1e5a5292bbf6e65
Tamara Bozich <myranae@pm.me>
2022-04-04 14:36:25 -0400

    adds vendor class and passes wave 1

---
+++ scratch.py
@@ -0,0 +1,3 @@
+# pytest --cov=swap_meet --cov-report html --cov-report term
+# open htmlcov/index.html
+# run from project root folder
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory # empty list by default
+    
+    def add(self, one_item):
+        self.inventory.append(one_item)
+        return one_item
+
+    def remove(self, one_item):
+        try:
+            self.inventory.remove(one_item)
+            return one_item
+        except ValueError:
+            return False
+        
