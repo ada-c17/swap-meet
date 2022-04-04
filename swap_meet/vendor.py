1dfcf21a0af881d966af5fba6adb65d95be41042
Lulu Sun <sunrey32@uw.edu>
2022-04-04 12:04:51 -0700

    Finished wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+    
+    def remove(self, new_item):
+        if new_item in self.inventory:
+            self.inventory.remove(new_item)
+            return new_item
+        return False
+    
+    def get_by_category(self, category):
+        result = []
+        for item in self.inventory:
+            if item.category == category:
+                result.append(item)
+        return result
