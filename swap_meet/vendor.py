c2c4248468e683718f9e56cfcf35405c8dbf977f
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 11:33:19 -0700

    created class Vendor as per Wave 1 instructions

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
+
 class Vendor:
-    pass
+    def __init__(self, inventory= []):
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            del self.inventory[self.inventory.index(item)]
+            return item
+        return False
