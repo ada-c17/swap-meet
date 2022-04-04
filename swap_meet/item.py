2e5046da7067898be67f0dbfb7886c447dabf3cf
Lulu Sun <sunrey32@uw.edu>
2022-04-04 15:44:24 -0700

    Finished wave 5. Solved all problems in previous wasves.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,21 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
     
     def __str__(self):
-        self = "Hello World!"
-        return self
+        return "Hello World!"
+
+    def condition_description(self):
+        description = ""
+        if self.condition == 0:
+            description = "EXCELLENT"
+        elif self.condition == 1:
+            description = "VERY GOOD"
+        elif self.condition == 2:
+            description = "GOOD"
+        elif self.condition == 3:
+            description = "FAIR"
+        else:
+            description = "POOR"
+        return f"The condition of this item is {description}"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,7 @@
 class Vendor:
-    def __init__(self, inventory = []):
+    def __init__(self, inventory = None):
+        if inventory is None:
+            inventory = []
         self.inventory = inventory
 
     def add(self, new_item):
@@ -19,11 +21,19 @@ class Vendor:
                 result.append(item)
         return result
 
-    def swap_items(self, their_vendor, my_item, their_item):
-        if my_item in self.inventory and their_item in their_vendor.inventory:
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
             self.add(their_item)
-            their_vendor.add(my_item)
+            other_vendor.add(my_item)
             self.remove(my_item)
-            their_vendor.remove(their_item)
+            other_vendor.remove(their_item)
+            return True
+        return False
+
+    def swap_first_item(self, other_vendor):
+        if self.inventory and other_vendor.inventory:
+            my_item = self.inventory[0]
+            their_item = other_vendor.inventory[0]
+            self.swap_items(other_vendor, my_item, their_item)
             return True
         return False
