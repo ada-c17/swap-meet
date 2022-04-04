87fb150c4b9b95694015ec5ad2d37963ce4f44ae
Uyen Vong <uvong92@gmail.com>
2022-04-04 12:43:29 -0700

    pass wave 4 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -37,3 +37,16 @@ class Vendor:
             other_vendor.remove(their_item)
             other_vendor.add(my_item)
             return True
+    
+    def swap_first_item(self, other_vendor):
+        
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        else:
+            my_first_item = self.inventory[0]
+            their_first_item = other_vendor.inventory[0]
+            self.remove(my_first_item)
+            self.add(their_first_item)
+            other_vendor.remove(their_first_item)
+            other_vendor.add(my_first_item)
+            return True
