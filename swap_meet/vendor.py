bae7171c1ec4c37499c2fb5f013450b9fb3f7abd
Diana <arantxa.codes@gmail.com>
2022-04-04 15:47:48 -0700

    Added get_by_category method and swap_items method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -11,3 +11,16 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+    
+    def get_by_category(self, category):
+        output = [Item for Item in self.inventory if category == Item.category]
+        return output
+        
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item)
+            friend.remove(their_item)
+            self.remove(my_item)
+            self.add(their_item)
+            return True
+        return False
