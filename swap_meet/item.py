f6354718e3c2de9ae543478e54c591f7f253b12e
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 15:15:39 -0700

    add condition as a constructor parameter for class Item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,13 @@
+from swap_meet.utils import CONDITION_RATINGS
 class Item:
-    def __init__(self, category=""):
-        self.category = category
 
-    def __str__(self):
+    def __init__(self, category="", condition=0):
+        self.category = category
+        self.condition = condition
+
+    @staticmethod
+    def __str__():
         return "Hello World!"
+    
+    def condition_description(self):
+        return CONDITION_RATINGS.get(self.condition, None)
