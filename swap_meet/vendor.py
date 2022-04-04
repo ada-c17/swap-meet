f45d8d35c0a1b3e58de49e20cb6d0893fbd70e97
Joan <jakovacs@protonmail.com>
2022-04-04 11:56:09 -0700

    Finished test wave 1 (+ .remove() method for Vendor)

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -4,3 +4,7 @@ class Vendor:
     def add(self, item):
         self.inventory.append(item)
         return item
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+        return item
