8fe0c23166baab077232dc260b83734937b0003e
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-04 15:24:09 -0700

    Passing 1 test for Wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,5 @@
 class Item:
-    pass
+
+    def __init__(self, category=''):
+        # Each 'Item' will have an attribute named 'category' that is an empty string by default
+        self.category = category
