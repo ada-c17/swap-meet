214af39c9a444609fdeb99c919b44e9772090b61
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-04-04 13:28:33 -0500

    Completed Wave_01, passes all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        try:
+            item_index = self.inventory.index(item)
+            return self.inventory.pop(item_index)
+        except ValueError:
+            return False
