33dbc49bdf9d2f1999a62bc3a7a6f5bc13e17757
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 16:11:53 -0700

    refactor instance method get_best_by_category to make use of get_by_category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,10 +2,12 @@ class Vendor:
 
     def __init__(self, inventory=[]):
         self.inventory = inventory
-    
+
+
     def add(self, item):
         self.inventory.append(item)
         return item
+
 
     def remove(self, item):
         if item in self.inventory:
@@ -13,18 +15,20 @@ class Vendor:
             return item
         return False
 
+
     def get_by_category(self, category):
         return [item for item in self.inventory if item.category == category]
+
 
     def swap_items(self, other, my_item, their_item):
         if my_item not in self.inventory or their_item not in other.inventory:
             return False
-            
         self.add(their_item)
         self.remove(my_item)
         other.add(my_item)
         other.remove(their_item)
         return True
+
 
     def swap_first_item(self, other):
         if not self.inventory or not other.inventory:
@@ -33,14 +37,15 @@ class Vendor:
             self.inventory[0], other.inventory[0] = other.inventory[0], self.inventory[0]
             return True
 
+
     def get_best_by_category(self, category):
-        items_in_category = [item for item in self.inventory if item.category == category]
 
         best_item = None
 
-        for item in items_in_category:
+        for item in self.get_by_category():
             if best_item is None or item.condition > best_item.condition:
                 best_item = item
         
         return best_item
+
 
