14b3e0b8c13fbcb8d69cf1d29db33ea0b0441e4a
tinghj17 <tinghj17@gmail.com>
2022-04-05 14:52:47 -0700

    Passed wave 04

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,12 +25,18 @@ class Vendor:
     
     def swap_items(self, friend, vender_item, friend_item):
         if vender_item in self.inventory and friend_item in friend.inventory:
-            self.inventory.remove(vender_item)
-            self.inventory.append(friend_item)
-            friend.inventory.remove(friend_item)
-            friend.inventory.append(vender_item)
+            self.remove(vender_item)
+            self.add(friend_item)
+            friend.remove(friend_item)
+            friend.add(vender_item)
             return True
         else: 
             return False
+    
+    def swap_first_item(self, friend):
+        if len(self.inventory) != 0 and len(friend.inventory) != 0:
+            first_item_vendor = self.inventory[0]
+            first_item_friend = friend.inventory[0]
+            return self.swap_items(friend, first_item_vendor, first_item_friend)
 
 
