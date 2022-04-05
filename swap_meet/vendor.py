8a22309323ccb59dd1284f44241e5de1ed1f2f5c
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-04-04 21:58:58 -0500

    Updated Vendor and Item classes to pass all tests in Wave 03 and Wave 04

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -10,13 +10,33 @@ class Vendor:
         return item
 
     def remove(self,item):
-        try:
+        if item not in self.inventory:
+            return False
+        else:
             item_index = self.inventory.index(item)
             return self.inventory.pop(item_index)
-        except ValueError:
-            return False
 
     def get_by_category(self, category):
         # Returns a list of Items in the inventory with that category
         items_by_category = [item for item in self.inventory if item.category == category]
         return items_by_category
+    
+    def swap_items(self, vendor, my_item, their_item):
+        if not self.remove(my_item):
+            return False
+        elif not vendor.remove(their_item):
+            self.add(my_item)
+            return False
+        else:
+            self.remove(my_item)
+            vendor.add(my_item)
+
+            vendor.remove(their_item)
+            self.add(their_item)
+            return True
+    
+    def swap_first_item(self, vendor):
+        if len(self.inventory) == 0 or len(vendor.inventory) == 0:
+            return False
+        self.swap_items(vendor, self.inventory[0], vendor.inventory[0])
+        return True
