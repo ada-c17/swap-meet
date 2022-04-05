856f2036b97c02b4b1ea798fbd7880accefb5b9a
mit <mit.magh@gmail.com>
2022-04-04 22:31:11 -0700

    Defined Class Vendor and End of Wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+        
+    
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
