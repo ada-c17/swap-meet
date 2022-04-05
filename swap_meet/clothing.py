f84baa2222144621bde5201f1fa55c03727b0411
Tamara Bozich <myranae@pm.me>
2022-04-05 10:52:18 -0400

    pass 1st integration test and pass wave 4

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,7 @@
 class Clothing:
+    def __init__(self):
+        self.category = "Clothing"
+        
+    def __str__(self):
+        return "The finest clothing you could wear."
     pass
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,9 @@
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory # empty list by default
+    def __init__(self, inventory=None):
+        if not inventory:
+            self.inventory = []
+        else:
+            self.inventory = inventory
     
     def add(self, one_item):
         self.inventory.append(one_item)
@@ -16,3 +19,24 @@ class Vendor:
     def get_by_category(self, category_to_check):
         list_by_cat = [item for item in self.inventory if item.category == category_to_check]
         return list_by_cat
+
+    def swap_items(self, swap_friend, my_item, their_item):
+        if my_item not in self.inventory:
+            return False
+        if their_item not in swap_friend.inventory:
+            return False
+        self.remove(my_item)
+        swap_friend.add(my_item)
+        swap_friend.remove(their_item)
+        self.add(their_item)
+        return True
+
+    def swap_first_item(self, swap_friend):
+        if not self.inventory or not swap_friend.inventory:
+            return False
+        my_item = self.inventory.pop(0)
+        their_item = swap_friend.inventory.pop(0)
+        self.add(their_item)
+        swap_friend.add(my_item)
+        return True
+        
