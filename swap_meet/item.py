92fcf90217cec55c27fa2b659b52ec466bd74b16
Natalya <chshebetun@gmail.com>
2022-04-05 15:22:44 -0700

    Add a new argument and method to item.py

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,18 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
     
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        description = {
+            0: "new", 
+            1: "barely used",
+            2: "good for a couple of days",
+            3: "found it in Paris",
+            4: "light blue color",
+            5: "has weird smell"
+            }
+        return description[self.condition]
