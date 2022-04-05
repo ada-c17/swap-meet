6e8bfbcf2475f972fb49f2bdb5499aa854bbaf81
Esther Annorzie <estherannorzie@gmail.com>
2022-04-04 22:50:08 -0400

    passed wave 2 unit tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,3 @@
 class Item:
-    def __init__(self, category=None):
-        if not category:
-            category = []
-    
-    def get_by_category(self):
-        pass
+    def __init__(self, category=""):
+        self.category = category
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,15 +3,20 @@ class Vendor:
         if not inventory:
             inventory = []
         self.inventory = inventory
-        
     
     def add(self, item):
         self.inventory.append(item)
         return item
     
-
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             return item
         return False
+
+    def get_by_category(self, specific_category):
+        category_matches = []
+        for item in self.inventory:
+            if item.category == specific_category:
+                category_matches.append(item)
+        return category_matches
