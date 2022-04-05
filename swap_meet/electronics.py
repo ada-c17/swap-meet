3451af474ca709e953967e2cb13cdfd7207429c9
Emily Saeli <emilysaeli@gmail.com>
2022-04-04 21:03:20 -0700

    Passing Wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,7 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+class Electronics(Item):
+    def __init__(self, category = "Electronics", condition = 0):
+        super().__init__(category, condition)
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
