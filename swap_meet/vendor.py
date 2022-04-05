25b5b3c5387d52c61d10c8f23f2abb0b4790a075
verasazonova <vera.zhokhova@gmail.com>
2022-04-04 21:16:25 -0700

    Create instance method for Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,6 @@
-class Vendor:
+from .item import Item
+
+class Vendor():
     # create inventory parameters, where deafalt is empty list
     def __init__(self,inventory = None):
         if inventory is None:
@@ -17,3 +19,11 @@ class Vendor:
                 self.inventory.remove(items)
                 return items
         return False
+
+    # create instance method which return a list of Items in the inventory with that category
+    def get_by_category(self,category):
+        list_of_items = []
+        for item in self.inventory:
+            if category == item.category:
+                list_of_items.append(item)
+        return list_of_items
