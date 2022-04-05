6501ba2a3462265b8dcdeedc62f44fd86a882a49
verasazonova <vera.zhokhova@gmail.com>
2022-04-04 21:15:24 -0700

    Create class Item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
+# from swap_meet.vendor import Vendor
+
 class Item:
-    pass
+    # create category parameters, where deafalt is empty string
+    def __init__(self, category = None):
+        if category is None:
+            category = ""
+        self.category = category
