c0eef6ce524ab0aad9786b083385f7b235998c73
Sreymompech <sanchhen2013@gmail.com>
2022-04-04 20:52:14 -0700

    In Vendor class of Wave 1, adding instance vendor method named get_by_category()

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,10 +1,14 @@
 # Wave 1
+from nis import cat
+from swap_meet.item import Item
+
+
 class Vendor:
     """
     attributes: inventory
     methods : add and remove
     """
-    
+
     def __init__(self, inventory = None):
         """Inventory is keyword argument that optionally pass in."""
         # assign empty list to inventory when the if statement is falsy. 
@@ -12,7 +16,8 @@ class Vendor:
         if not inventory:
             inventory = []
         self.inventory = inventory
-        
+        # self.obj_item = Item()
+
         
     def add(self, item):
         """Adding new item into inventory and returns the added item."""
@@ -26,4 +31,21 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+
+
+    def get_by_category(self, category):
+        """
+        - Adding item object into a list if category is in vendor's inventory
+        - Return empty list if inventory list is empty. Otherwise returns list of items in inventory.
+
+        """
+        items_list = []
+        if len(self.inventory) > 0:
+            # loop over inventory list to check each item object's category
+            # add item object into list if its category macthes to parameter
+            for item in self.inventory:
+                if item.category == category:
+                    items_list.append(item)
+            return items_list
+        return []
         
