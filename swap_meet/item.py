12a1e8515a2a7be1ecce608d4290783467243dcc
Marjana Khatun <marjana1816@gmail.com>
2022-04-05 13:55:52 -0700

    Wave1-3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,14 @@
+# from .vendor import Vendor
+
 class Item:
-    pass
+  
+    def __init__(self,category = ""):
+        self.category = category
+
+    def __str__(self):
+        return "Hello World!"
+
+    # def swap_items():
+
+my_item = Item()
+their_item = Item()
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,47 @@
+from .item import Item
+
 class Vendor:
-    pass
+    def __init__(self,inventory = []):
+        self.inventory = inventory
+
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+           
+
+    def get_by_category(self,category):
+        category_list = []
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+        return category_list
+            
+
+    def swap_items(self,vendor,my_item,their_item):
+        if my_item in self.inventory:
+            self.inventory.remove(my_item)
+            vendor.inventory.append(my_item)
+            return True
+        else:
+            return False
+
+        if their_item in vendor.inventory:
+            vendor.inventory.remove(their_item) 
+            self.inventory.append(their_item)
+            return True
+        else:
+            return False
+
+
+
+vendor = Vendor()
+
+        
