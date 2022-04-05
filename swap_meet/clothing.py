975aa01a86cd419cfc80308183f93f365f6ef296
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-04-05 13:48:43 -0400

    completed wave_05 tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,11 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+    def __init__(self, condition=0, category="Clothing"):
+        super().__init__(condition)
+        self.category = category
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
+    
+    
