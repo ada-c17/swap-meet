98e7eaf3459362b962106d0b4b1ee49250215ede
Andrea Garcia <andrygzt@gmail.com>
2022-04-05 16:57:10 -0700

    the one when wave 01 and 02 passes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,16 +2,24 @@ class Vendor:
     def __init__(self, inventory =[]):
         self.inventory = inventory 
 
-    def add(self, item_added):
-        new_item = self.inventory.append(item_added)
-        return item_added
+    def add(self, item):
+        self.inventory.append(item)
+        return item
 
-    def remove(self, item_removed):
-        if item_removed in self.inventory:
-            self.inventory.remove(item_removed)
-            return item_removed
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
         else:
             return False
         
+
+    def get_by_category (self, category):   
+        same_category = []
+        for item in self.inventory:
+            if category == item.category:
+                same_category.append(item)
+            
+        return same_category
 
 
