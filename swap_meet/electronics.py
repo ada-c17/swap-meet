06bdb5303795445deb7dd6efa3bd06b9c47b3bb5
Christina Webber <chrisswebb@yahoo.com>
2022-04-05 16:37:14 -0500

    removed category arguments from item child classes

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,8 +2,8 @@ from swap_meet.item import Item
 
 class Electronics(Item):
 
-    def __init__(self, category = "Electronics", condition = 0):
-        super().__init__(category, condition)
+    def __init__(self, condition = 0):
+        super().__init__("Electronics", condition)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
