c3cf7917d46c7102c8bafd1f34ef806765567f38
Megan Korling <korlingm@gmail.com>
2022-04-05 16:02:01 -0700

    added questions as comments to ask

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,7 +14,6 @@ class Vendor:
         if item in self.inventory:
             self.inventory.remove(item)
             return item
-        # Wave 1 directions
         return False
 
     def get_by_category(self, category):
@@ -24,33 +23,32 @@ class Vendor:
 
     def swap_items(self, second_person, user_item, second_item):
         if user_item in self.inventory and second_item in second_person.inventory:
-            self.add(second_item)
+            #self.add(second_item)
+            self.add(second_person.remove(second_item))
             self.remove(user_item)
             second_person.add(user_item)
-            second_person.remove(second_item)
+            #second_person.remove(second_item)
             return True
         return False
 
     def swap_first_item(self, second_person):
         if len(self.inventory) > 0 and len(second_person.inventory) > 0:
-            self.swap_items(second_person, self.inventory[0], second_person.inventory[0])
-            return True
-        return False
+            return self.swap_items(second_person, self.inventory[0], second_person.inventory[0])
+            #return True
+        #return False
 
     def get_best_by_category(self, best_cat):
-        cat_list = self.get_by_category(best_cat)
-        # max goes through cat_list, where each element is passed through the lambda func
+        items_list = self.get_by_category(best_cat)
+        # max goes through items_list, where each element is passed through the lambda func
         # lambda func returns the float value from condition attr.
         # based on that value, max finds the max and returns the element
         # How does it know to return the element and not x.condition value
-        best_item = max(cat_list, default=None, key=lambda x: x.condition)
+        best_item = max(items_list, default=None, key=lambda x: x.condition)
         return best_item
 
     def swap_best_by_category(self, other, my_priority, their_priority):
         my_item = self.get_best_by_category(their_priority)
         their_item = other.get_best_by_category(my_priority)
-        # if my_item and their_item:
-        #     self.swap_items(other, my_item, their_item)
-        #     return True
-        # return False
+        # is it best practice to return a function like this?
+        # swap_items already returns t/f, so i thought, why repeat that here?
         return self.swap_items(other, my_item, their_item)
