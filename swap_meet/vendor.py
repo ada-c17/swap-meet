2f362960b80ed7940bdd2161b98c7f068854ff85
Marlyn <marlynlopezz@gmail.com>
2022-04-04 20:06:39 -0700

    First wave completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if not inventory:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self, add_item):
+        add = self.inventory.append(add_item)
+        return add_item
+
+    def remove(self, sub_item):
+        if sub_item in self.inventory:
+            self.inventory.remove(sub_item)
+            return sub_item
+        else:
+            return False
+    
+
+        
+        
+
+
