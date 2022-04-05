636012a1df300654f76f2659adcd12cbf552d6cc
Lin Liu <lin_6066@yahoo.com>
2022-04-04 20:23:14 -0700

    add/update constructor

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,16 @@
 class Item:
-    def __init__(self, category="", condition = "0"):
+    def __init__(self, category="", condition = 0):
         self.category = category
         self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        condition_desc = ["please check with customer service",
+                          "worn/used few times",
+                          "no flaws",
+                          "good",
+                          "excellent",
+                          "new",]
+        return condition_desc[int(self.condition)]
