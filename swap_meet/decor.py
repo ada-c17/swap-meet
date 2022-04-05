fcf26d7496e86ac2eb87cef9153653964bb9ca69
Sreymompech <sanchhen2013@gmail.com>
2022-04-05 13:30:31 -0700

    In decor.py file, creating Decor class as child class of Item class that inherits the attributes and methods

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,18 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+class Decor(Item):
+    """Child class that inherits attributes and methods from Item class as parent class:"""
+
+    # Wave 5
+    def __init__(self, category = "Decor", **kargs):
+        """Inheriting all attributs from Item class"""
+        super().__init__(category = "Decor", **kargs)
+
+
+    def __str__(self):
+        """Returning value in string format by override inherited method from Item class."""
+        return str("Something to decorate your space.")
+
+
+    def condition_description(self):
+        """Returing inherited method from Item class."""
+        return super().condition_description()
