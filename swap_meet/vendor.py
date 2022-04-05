6c7acf32b830b68da09ff31447197ade4927c936
Diana <arantxa.codes@gmail.com>
2022-04-05 14:35:16 -0700

    Cleaned up if statements

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -29,22 +29,22 @@ class Vendor:
             return True
     
     def swap_first_item(self, other_vendor):
-        if len(self.inventory) > 0 and len(other_vendor.inventory) > 0:
+        if len(self.inventory) and len(other_vendor.inventory):
             self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
             return True
     
     def get_best_by_category(self, category):  
         try: 
-            max_value = max(Item.condition for Item in self.inventory if Item.category == category)
-            for Item in self.inventory:
-                if Item.category == category and Item.condition == max_value:
-                    return Item
+            max_value = max(item.condition for item in self.inventory if item.category == category)
+            for item in self.inventory:
+                if item.category == category and item.condition == max_value:
+                    return item
         except:
             return None
     
     def swap_best_by_category(self, other, my_priority, their_priority):
         my_inventory = self.get_by_category(their_priority)
         other_inventory = other.get_by_category(my_priority)
-        if len(my_inventory) > 0 and len(other_inventory) > 0:
+        if len(my_inventory) and len(other_inventory):
             self.swap_items(other, self.get_best_by_category(their_priority), other.get_best_by_category(my_priority))
             return True    
