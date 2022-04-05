2c1e7fd4d72fa02bcf49bf9947e0c75aa98522ac
Joanna Dudley <jfdudleybmc@gmail.com>
2022-04-05 15:37:59 -0400

    Created new classes Electronics, Decor, and Clothing, inheriting from Items class. Created condition_description function, passing all wave 5 tests.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,14 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+
+class Decor(Item):
+    def __init__(self, category = "Decor", condition = 0):
+        super().__init__(category, condition)
+        # self.condition = condition
+        # self.category = category
+
+    def __str__(self):
+        return "Something to decorate your space."
+    
+    def condition_description(self):
+        return super().condition_description()
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,13 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category = "Electronics", condition = 0):
+        super().__init__(category, condition)
+        # self.condition = condition
+        # self.category = category
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
+
+    def condition_description(self):
+        return super().condition_description()
