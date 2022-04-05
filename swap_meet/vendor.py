dd44fc3c7d912ebcb036c457a5e5a9f46624c417
Diana <arantxa.codes@gmail.com>
2022-04-04 17:08:33 -0700

    Added swap_first_item method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,12 +15,21 @@ class Vendor:
     def get_by_category(self, category):
         output = [Item for Item in self.inventory if category == Item.category]
         return output
-        
-    def swap_items(self, friend, my_item, their_item):
-        if my_item in self.inventory and their_item in friend.inventory:
-            friend.add(my_item)
-            friend.remove(their_item)
-            self.remove(my_item)
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            other_vendor.add(my_item)
             self.add(their_item)
+            other_vendor.remove(their_item)
+            self.remove(my_item)
+            return True
+        return False
+    
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) > 0 and len(other_vendor.inventory) > 0:
+            self.add(other_vendor.inventory[0]) 
+            other_vendor.add(self.inventory[0])  
+            self.remove(self.inventory[0]) 
+            other_vendor.remove(other_vendor.inventory[0])
             return True
         return False
