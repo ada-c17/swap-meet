289e87d6b29ec1de78f1af842b51ffc1f44fb598
Diana <arantxa.codes@gmail.com>
2022-04-04 19:14:12 -0700

    Added condition description method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,17 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
+
     def __str__(self):
         return "Hello World!"
 
+    def condition_description(self):
+        description = { 0: "xoxo let it go",
+                        1: "florals? for spring?",
+                        2: "moderately fetch",
+                        3: "bend and snap",
+                        4: "star dazzle award",
+                        5: "what dreams are made of"
+        }
+        return description[self.condition]
