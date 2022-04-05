8e21a3de8afeb870051998871d7cec1f96f7cc13
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-04-05 16:23:43 -0700

    Completed instance method swap_first_item

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,4 @@
-# from swap_meet.item import Item
+from swap_meet.item import Item
 
 class Vendor:
     #insert doc strings
@@ -20,7 +20,8 @@ class Vendor:
     
     def remove(self, item):
         '''
-        Removes item from attribute inventory if item in list, returns item; else returns False.
+        Removes item from attribute inventory if item in list, returns item. 
+        If item not in self.inventory, returns False.
         ''' 
         if item in self.inventory:
             self.inventory.remove(item)
@@ -29,25 +30,42 @@ class Vendor:
 
     def get_by_category(self, category):
         '''
-        Returns list of items from inventory whose attribute category equals input category string.
+        Returns list of items from inventory whose attribute category == input category string.
         '''
         category_list = []
         for item in self.inventory:
             if category == item.category:
                 category_list.append(item)
         return category_list
-
+        
     def swap_items(self, vendor, my_item, their_item):
         '''
-        Swaps (removes/appends) items from instance inventory and vendor inventory; returns True.
+        Swaps (removes/appends) input items (my_item, their_item) from instance inventory and vendor inventory; returns True.
+        If my_item, their_item not in self.inventory and vendor.inventory, respsectively, returns False.
         '''
         if my_item not in self.inventory or their_item not in vendor.inventory:
             return False
         
-        self.inventory.remove(my_item)
+        #can/cannot invoke previously made instances methods remove()/add() on instances self and vendor b/c of return?
+        self.inventory.remove(my_item)   # have you overwritten python fnx for remove?
         vendor.inventory.remove(their_item)
         self.inventory.append(their_item)
         vendor.inventory.append(my_item)
 
         return True
+
+    def swap_first_item(self, vendor):
+        '''
+        Swaps (removes/appends) first item from self.inventory and vendor.inventory, returns True
+        If self.inventory or vendory.inventory empty, returns False'''
+        if not self.inventory or not vendor.inventory:
+            return False
+        
+        first_item_self = self.inventory.pop(0)  #want to return value here and assign to variable
+        first_item_vendor = vendor.inventory.pop(0)
+        self.inventory.append(first_item_vendor)
+        vendor.inventory.append(first_item_self)
+
+        return True
+
         
