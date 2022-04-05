3023216b9f08bf136aa918ddf00e352c0ddcabda
Christina Webber <chrisswebb@yahoo.com>
2022-04-05 12:29:05 -0500

    no longer need a list copy in vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,28 +1,27 @@
-# from swap_meet.item import Item
 from operator import attrgetter
 import copy
 
 class Vendor:
     
-    def __init__(self, inventory = []):
-        self.inventory = copy.copy(inventory)
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
     
-    def add(self, it):
-        self.inventory.append(it)
-        return it
+    def add(self, item):
+        self.inventory.append(item)
+        return item
     
-    def remove(self, it):
-        if it in set(self.inventory):
-            self.inventory.remove(it)
-            return it
+    def remove(self, item):
+        if item in set(self.inventory):
+            self.inventory.remove(item)
+            return item
         else:
             return False
     
     def get_by_category(self, category):
         cat_list = []
-        for i in self.inventory:
-            if i.category == category:
-                cat_list.append(i)
+        for item in self.inventory:
+            if item.category == category:
+                cat_list.append(item)
         return cat_list
     
     def swap_items(self, other, my_item, their_item):
@@ -42,10 +41,7 @@ class Vendor:
     
     def swap_first_item(self, other):
         if len(self.inventory) >= 1 and len(other.inventory) >= 1:
-            other.add(self.inventory[0])
-            self.remove(self.inventory[0])
-            self.add(other.inventory[0])
-            other.remove(other.inventory[0])
+            self.swap_items(other, self.inventory[0], other.inventory[0])
             return True
         else:
             return False
@@ -55,20 +51,17 @@ class Vendor:
         if len(items_in_category) < 1:
             return None
         best_condition = 0
-        for i in items_in_category:
-            if i.condition > best_condition:
-                best_condition = i.condition
-                best_item = i
+        for item in items_in_category:
+            if item.condition > best_condition:
+                best_condition = item.condition
+                best_item = item
         return best_item
 
     def swap_best_by_category(self, other, my_priority, their_priority):
         my_choice = other.get_best_by_category(my_priority)
         their_choice = self.get_best_by_category(their_priority)
         if my_choice and their_choice:
-            self.remove(their_choice)
-            other.remove(my_choice)
-            other.add(their_choice)
-            self.add(my_choice)
+            self.swap_items(other, their_choice, my_choice)
             return True
         else:
             return False
