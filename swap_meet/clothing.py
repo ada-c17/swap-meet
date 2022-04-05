b2f4021018a394036b17726dbe57622850395340
Kiana <kianashui@gmail.com>
2022-04-05 15:00:38 -0700

    refactored Clothing, Decor, and Electronics classes to inherit attributes from Item

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,9 +1,8 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, condition=0, age=0):
+    def __init__(self, category = "", condition=0, age=0):
+        super().__init__(category, condition, age)
         self.category = "Clothing"
-        self.condition = condition
-        self.age = age
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,9 +1,8 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, condition=0, age=0):
+    def __init__(self, category="", condition=0, age=0):
+        super().__init__(category, condition, age)
         self.category = "Electronics"
-        self.condition = condition
-        self.age = age
     def __str__(self):
         return "A gadget full of buttons and secrets."
