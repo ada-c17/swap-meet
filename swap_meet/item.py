d96c34049a5243142f2de7c493bb58a47da12548
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-04-04 22:02:42 -0700

    Wave 3 completed, all tests + integration tests passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,9 @@
 class Item:
     def __init__(self, category = ""):
         self.category = category
+    
+    def __str__(self):
+        return "Hello World!"
+    
+    
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,3 +22,12 @@ class Vendor:
             if category == item.category:
                 category_items.append(item)
         return category_items
+        
+    def swap_items(self, friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in friend.inventory:
+            return False
+        self.remove(my_item)
+        friend.add(my_item)
+        friend.remove(their_item)
+        self.add(their_item)
+        return True
