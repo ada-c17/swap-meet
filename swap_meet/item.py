51ea341265058423e908d48f9ac9eea3a7689546
Jodi Denney <iarrived@gmail.com>
2022-04-05 16:06:35 -0700

    Passed Wave 5- clothing, decor, electronics child classes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,21 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "You gotta see it to beleive it"
+        elif self.condition == 1:
+            return "A hand-me-down many, many times over"
+        elif self.condition == 2:
+            return "Like reliving your college dorm"
+        elif self.condition == 3:
+            return "A gift from my ex - he was ok"
+        elif self.condition == 4:
+            return "Like new - not new but like it"
+        elif self.condition == 5:
+            return "The Beyonce of objects"
