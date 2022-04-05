62004b7880128484043e10e3bfef334c9747504f
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-04-04 18:00:21 -0700

    Wave 2 completed, all tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,4 @@
-from pytest import Item
-
+# from swap_meet.item import Item
 
 class Vendor:
     def __init__(self, inventory = None):
@@ -17,3 +16,9 @@ class Vendor:
             return item
         return False
     
+    def get_by_category(self, category):
+        category_items =[]
+        for item in self.inventory:
+            if category == item.category:
+                category_items.append(item)
+        return category_items
