d18b4aeca5ed8a0c7263b12c1643f3aa6705d194
Jenny <jfperham@gmail.com>
2022-04-05 11:12:16 -0700

    made it through wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,9 @@
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
         self.inventory = inventory
+        if inventory is None:
+            self.inventory = []
+       
 
     def add(self, new_item):
         self.inventory.append(new_item)
@@ -22,10 +25,42 @@ class Vendor:
             if items == []:
                 return False
         return items
-        
-    # def swap_items(self, Vendor(friends_inventory), Item(my_item), Item(their_item)):
-        pass
-#     It removes the my_item from this Vendor's inventory, and adds it to the friend's inventory
-# It removes the their_item from the other Vendor's inventory, and adds it to this Vendor's inventory
-# It returns True
-# If this Vendor's inventory doesn't contain my_item or the friend's inventory doesn't contain their_item, the method returns False
+
+    #  result = fatimah.swap_items(jolie, item_b, item_d)
+    # passing in instance of Vendor, instance of Item, instance of Item
+    def swap_items(self, vendor_friend, my_item, their_item):
+        '''
+        input: 3 parameters: One instance of vendor class (a friend), two instances of item classes.
+            The items will be swapped: first item will be added to current vendor, second item will be added to 2nd vendor
+            ouptut: True or False (False if items that should be swapped are not within their vendor's inventory)
+        '''
+        if my_item in self.inventory and their_item in vendor_friend.inventory:
+            #vendor_friend.inventory.append(my_item)
+            vendor_friend.add(my_item)
+            #self.inventory.remove(my_item)
+            self.remove(my_item)
+            #self.inventory.append(their_item)
+            self.add(their_item)
+            #vendor_friend.inventory.remove(their_item)
+            vendor_friend.remove(their_item)
+            return True
+        else: 
+            return False
+
+# Here, both instances are called into the function:
+
+# def test_get_student_with_more_classes_student_a():
+#     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
+#     ada = Student(
+#         "Ada Lovelace",
+#         "sophomore",
+#         ["mathematics", "foundations of computing"]
+#     )
+#     # act
+#     student_a = charles.get_student_with_more_classes(ada, charles)
+#     assert student_a == ada    
+
+# def get_student_with_more_classes(self, student_a, student_b):
+#         if student_a.get_num_classes() > student_b.get_num_classes():
+#             return student_a
+#         return student_b
