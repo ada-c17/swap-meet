b8f040b8e7a34186b49d695f1d41be83ac620e56
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 17:44:22 -0700

    add swap_best_by_category instance method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -65,3 +65,15 @@ class Vendor:
                     best_item = item
         
         return best_item
+
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        
+        my_item = self.get_best_by_category(their_priority)
+        their_item = other.get_best_by_category(my_priority)
+
+        if not my_item or not their_item:
+            return False
+        
+        else:
+            return self.swap_items(other, my_item, their_item)
