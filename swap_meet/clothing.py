244ab6f20d5374a96d50f3a58344bf5438f0c19a
Sreymompech <sanchhen2013@gmail.com>
2022-04-05 13:28:24 -0700

    In clothing.py file, creating Clothing class as child class of Item class that inherits the attributes and method

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,19 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+class Clothing(Item):
+    """Child class that inherits attributes and methods from Item class as parent class:"""
+
+    # Wave 5
+    def __init__(self, category = "Clothing", **kargs):
+        """Inheriting all attributs from Item class"""
+        super().__init__(category = "Clothing", **kargs)
+
+
+    def __str__(self):
+        """Returning value in string format by override inherited method from Item class."""
+        return str("The finest clothing you could wear.")
+
+
+    def condition_description(self):
+        """Returing inherited method from Item class."""
+        return super().condition_description()
+    
