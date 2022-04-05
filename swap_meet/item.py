bce8666bdd7465b42002cef5c97b57c4f04e1787
Natascha <njenkns@gmail.com>
2022-04-05 10:12:13 -0700

    completed wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,5 @@
 class Item:
-    pass
+    def __init__(self, category=""): # str are immutable, so okay to use as default parameter
+        self.category = category
+    
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,26 @@
 class Vendor:
     '''Creating Vendor class that has an inventory attribute which is an optional argument. The add instance method adds a new item to inventory and the remove instance method
     removes an item if that item is found in the inventory list. If item is not found in list to remove, False is returned. '''
-    def __init__(self, inventory=[]): 
-        self.inventory = inventory
+    def __init__(self, inventory=None): 
+        if inventory is None: # shouldn't use mutable type as default parameter, so use None and then assign to empty list # O(1)
+            inventory = [] 
+        self.inventory = inventory 
+
+    def add(self, item):
+        self.inventory.append(item) # O(1)
+        return item 
     
-    def add(self, new_item):
-        self.inventory.append(new_item)
-        return new_item 
-    
-    def remove(self, rem_item):
-        if rem_item in self.inventory:
-            self.inventory.remove(rem_item)
-            return rem_item
+    def remove(self, item):
+        if item in self.inventory: # O(1)
+            self.inventory.remove(item) # O(1) 
+            return item
         return False 
+
+    def get_by_category(self, category):
+        items_with_categories = [] 
+        for item in self.inventory: # O(n)
+            if item.category == category: # O(1)
+                items_with_categories.append(item) # O(1)      
+        return items_with_categories 
+
+        
