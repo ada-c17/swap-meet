21d557af44af0d970ccaef50848109fb9ff63bcf
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-04-05 11:03:16 -0500

    Updated Clothing, Decor, and Electronics class. Passes all tests form Wave 05

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,19 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
     
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if 0 < self.condition <= 1:
+            return "Falling Apart"
+        elif 1 < self.condition <= 2:
+            return "Heavily Used"
+        elif 2 < self.condition <= 3:
+            return "Fair"
+        elif 3 < self.condition <= 4:
+            return "Good"
+        elif 4 < self.condition <= 5:
+            return "Like New"
