b4781c4a66e2e7454a88b1177d8d8106a8ac29bb
Danielle <dmbecker24@gmail.com>
2022-04-05 14:22:44 -0700

    Add swap_items method to vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,3 @@
-from .item import Item
 
 class Vendor:
     def __init__(self, inventory=None):
@@ -24,5 +23,21 @@ class Vendor:
             if item.category == category:
                 items.append(item)
         return items
+    
+    def swap_items(self, vendor_friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor_friend.inventory:
+            return False
+
+        self.inventory.remove(my_item)
+        vendor_friend.inventory.remove(their_item)
+        if their_item.category not in self.inventory:
+            self.inventory.append(their_item)
+            vendor_friend.inventory.append(my_item)
+        return True
+# It removes the my_item from this Vendor's inventory, and adds it to the friend's inventory
+# It removes the their_item from the other Vendor's inventory, and adds it to this Vendor's inventory
+# It returns True
+# If this Vendor's inventory doesn't contain my_item or the friend's inventory doesn't contain 
+# their_item, the method returns False
 
 
