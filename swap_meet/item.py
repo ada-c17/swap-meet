bdd81d1217fcc47d4f50b4c2cff342b9bb3f4adb
Thu Vuong <tavuongg@gmail.com>
2022-04-04 20:35:30 -0700

    completed wave 3-5, passed tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,23 @@
 class Item:
-    def __init__(self, category=None):
-        if category:
+    def __init__(self, category=None, condition=0):
+        if category or condition:
             self.category = category
+            self.condition = condition
         else:
             self.category = ""
+
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        descriptions = {
+            0: "You tell me?",
+            1: "Do you really want this?",
+            2: "This might be nice in the corner on a shelf somewhere.",
+            3: "It's better than nothing!",
+            4: "I used it but I didn't really use it",
+            5: "Just as good as the store but you pay less",
+        }
+
+        return descriptions[self.condition]
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,3 +28,25 @@ class Vendor:
         category_items = [item for item in self.inventory if item.category == category]
         return category_items
 
+    def swap_items(self, friend, my_item, friend_item):
+        if my_item not in self.inventory or friend_item not in friend.inventory:
+            return False
+
+        self.remove(my_item)
+        friend.add(my_item)
+        friend.remove(friend_item)
+        self.add(friend_item)
+
+        return True
+    
+    def swap_first_item(self, friend):
+
+        if not self.inventory or not friend.inventory:
+            return False
+        
+        my_item = self.inventory.pop(0)
+        friend.add(my_item)
+        friend_item = friend.inventory.pop(0)
+        self.add(friend_item)
+
+        return True
