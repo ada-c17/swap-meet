7f9db2b3c04d9243f5b4f0868395ba4e02bfa8ad
Uyen Vong <uvong92@gmail.com>
2022-04-04 18:35:18 -0700

    refactor swap_first_item

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,8 +27,7 @@ class Vendor:
                 categorized_items.append(item)
         return categorized_items
 
-    def swap_items(self, other_vendor, my_item, their_item):
-        
+    def swap_items(self, other_vendor, my_item, their_item):  
         if my_item not in self.inventory or \
             their_item not in other_vendor.inventory:
             return False
@@ -40,25 +39,19 @@ class Vendor:
             return True
     
     def swap_first_item(self, other_vendor):
-        
         if not self.inventory or not other_vendor.inventory:
             return False
         else:
             my_first_item = self.inventory[0]
             their_first_item = other_vendor.inventory[0]
-            self.remove(my_first_item)
-            self.add(their_first_item)
-            other_vendor.remove(their_first_item)
-            other_vendor.add(my_first_item)
-            return True
+            return self.swap_items(other_vendor, my_first_item, their_first_item)
 
-    def get_best_by_category(self, category):
+    def get_best_by_category(self, category):    
         if not self.get_by_category(category):
             return None
         return max(self.get_by_category(category), key=attrgetter("condition"))
         
     def swap_best_by_category(self, other, my_priority, their_priority):
-
         my_best_item = self.get_best_by_category(their_priority)
         their_best_item = other.get_best_by_category(my_priority)
 
