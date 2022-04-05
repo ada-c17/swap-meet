5ecf99bfca1be2b732835777c413a1a01d1584d5
thaimynguyen <thaimynguyen@gmail.com>
2022-04-04 19:30:53 -0700

    add an age attribute to the constructor

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,12 @@
 from swap_meet.utils import CONDITION_RATINGS
+
+
 class Item:
 
-    def __init__(self, category="", condition=0):
+    def __init__(self, category="", condition=0, age=0):
         self.category = category
         self.condition = condition
+        self.age = age
 
     @staticmethod
     def __str__():
