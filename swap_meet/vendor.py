44be2e39f44004bfc448e96975dc173d0d521364
Morgan <morganheuer@gmail.com>
2022-04-05 13:56:52 -0500

    add swap_items method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,3 +15,13 @@ class Vendor:
 
     def get_by_category(self, category):
         return [item for item in self.inventory if item.category == category]
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            vendor.inventory.append(my_item)
+            self.inventory.append(their_item)
+            vendor.inventory.remove(their_item)
+            return True
