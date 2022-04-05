074d3b6f346723016bd1a20e79a68e95ced337a0
Ruge Li <rugeli0605@gmail.com>
2022-04-04 17:24:38 -0700

    wave 1-2 completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,26 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+
+    def remove(self,item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
+    
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
+    
