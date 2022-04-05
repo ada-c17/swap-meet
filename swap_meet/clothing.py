d15fc55aeb33ffd839636d129ff8503a0bb3b21e
Sophia Wagner <sophiewagnerla@gmail.com>
2022-04-05 10:20:54 -0700

    Passed all W5 tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,3 +1,9 @@
-class Clothing:
-    pass
-# Adding my first comment to this project
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, condition=0):
+        self.category = "Clothing"
+        self.condition = condition
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,21 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if 0 <= self.condition < 1:
+            return f"The condition of this item is {self.condition} out of 5, so it's not in good shape."
+        if 1 <= self.condition < 2:
+            return f"The condition of this item is {self.condition} out of 5, so it's likely heavily used."
+        if 2 <= self.condition < 3:
+            return f"The condition of this item is {self.condition} out of 5, so it's had some wear and tear."
+        if 3 <= self.condition < 4:
+            return f"The condition of this item is {self.condition} out of 5, so it should be decent."
+        if 4 <= self.condition < 5:
+            return f"The condition of this item is {self.condition} out of 5, so it's in great shape!"
+        if self.condition == 5:
+            return f"The condition of this item is {self.condition} out of 5, so it's in mint condition!"
