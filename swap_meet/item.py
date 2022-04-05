bfe3f99f48f4ac649469989a9a8f6ff18b86d747
Sreymompech <sanchhen2013@gmail.com>
2022-04-05 13:24:53 -0700

    In Item class of Wave 5, adding condition attribute in __init__() and method condition_description()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,22 +1,35 @@
-# Wave 2
-
-#from swap_meet.vendor import Vendor
 class Item:
     """
-    attributes: category
-    methods: get_by_category
+    attributes: category and conditon
+    methods: __str__() and condition_description()
     """
 
-    def __init__(self, category = None):
-        """category is keyword argument that optionally pass in."""
-        # assign empty string to category when the if statement is falsy. 
-        # Otherwise, assign category as value of object's category.
+    # Wave 2, create category attribute
+    # Wave 5, create condition attribute
+    def __init__(self, category = None, condition = None):
+        """category and condition are keyword arguments that optionally pass in."""
         if category is None:
             category = ""
+        if condition is None:
+            condition = 0
         self.category = category
+        self.condition = condition
 
 
+    # Wave 3
     def __str__(self):
-        """Return string with 'Hellow World!' """
+        """Return value in string format """
         return str("Hello World!")
 
+
+    # Wave 5
+    def condition_description(self):
+        """Return item description base on its condition."""
+        
+        if self.condition == 0:
+            return str("New condition")
+
+        elif self.condition < 4:
+            return str("Still useable")
+
+        return str("Old or heavily used")
