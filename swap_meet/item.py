10faf802943c793e9f5b7dc9c51f3e82723b6cda
tinghj17 <tinghj17@gmail.com>
2022-04-04 21:39:26 -0700

    Changed item and vendor passed wave 02

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,6 +2,4 @@ class Item:
     def __init__(self, category = ""):
         self.category = category
 
-    def get_by_category(self, chosen_category):
-        pass
 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,11 +1,9 @@
-from operator import inv
-
-
 class Vendor:
     def __init__(self, inventory = None):
         if inventory == None: 
             inventory = []
         self.inventory = inventory
+
     
     def add(self, item_added):
         self.inventory.append(item_added)
@@ -17,4 +15,11 @@ class Vendor:
             return item_removed
         else:
             return False
-    
+
+    def get_by_category(self, chosen_category):
+        items_in_chosed_category = []
+        for item in self.inventory:
+            if item.category == chosen_category:
+                items_in_chosed_category.append(item)
+        return items_in_chosed_category
+
