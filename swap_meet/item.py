6831a05fc7e2195078f92fcc9450e3d5411aab45
verasazonova <vera.zhokhova@gmail.com>
2022-04-04 22:36:39 -0700

    Some refactoring to wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,4 +5,11 @@ class Item:
     def __init__(self, category = None):
         if category is None:
             category = ""
-        self.category = category
+        self.category = category      
+
+    # return string, when instance is assign
+    def __repr__(self):
+        return self.category
+
+# item_a = Item(category="clothing")
+# print(item_a)
