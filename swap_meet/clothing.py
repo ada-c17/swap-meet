b072b6de6697f3b00eeb3509e2fb60e42fbc95e3
Olena <olenazachkow@gmail.com>
2022-04-04 21:35:40 -0700

    wave 5 and integration tests 1,2,3 passed

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,10 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+
+class Clothing(Item):
+    def __init__(self,category="Clothing",condition=0):
+        self.category=category
+        self.condition=condition
+
+    def __str__(self):
+        return (f"The finest clothing you could wear.")     
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,27 @@
 #from swap_meet.vendor import Vendor
 
 class Item:
-    def __init__(self,category=""):
-        #super().__init__(inventory)
+    def __init__(self,category="",condition=0):
         self.category=category
+        self.condition=condition
 
     def __str__(self):
         return (f"Hello World!")   
+
+    def condition_description(self): 
+        cond_description=self.condition
+        if cond_description==0:
+            return ("very bad")
+        elif cond_description==1:
+            return("moderate")
+        elif cond_description==2:
+            return("normal")
+        elif cond_description==3:
+            return("acceptable")    
+        elif cond_description==4:
+            return ("good")
+
+        return ("very very good")    
+
+
 
