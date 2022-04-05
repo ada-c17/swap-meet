75ad02f4a34933f7279a48a1c00a90653050d3c4
Uyen Vong <uvong92@gmail.com>
2022-04-04 18:28:46 -0700

    add test_item_conditions_have_accurate_description to increase code coverage

--- tests/unit_tests/test_wave_05.py
+++ tests/unit_tests/test_wave_05.py
@@ -52,3 +52,20 @@ def test_items_have_condition_descriptions_that_are_the_same_regardless_of_type(
         assert item.condition_description() == one_condition_description
 
     assert one_condition_description != five_condition_description
+
+def test_item_conditions_have_accurate_description():
+    items = [
+        Clothing(condition=5),
+        Decor(condition=4),
+        Electronics(condition=3),
+        Decor(condition=2),
+        Electronics(condition=1),
+        Clothing(condition=0)
+    ]
+
+    assert items[0].condition_description() == "Like new"
+    assert items[1].condition_description() == "Very good" 
+    assert items[2].condition_description() == "Good" 
+    assert items[3].condition_description() == "Acceptable" 
+    assert items[4].condition_description() == "Ugly" 
+    assert items[5].condition_description() == "As is" 
