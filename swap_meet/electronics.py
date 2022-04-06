5f0f900109d046fe734ef6928c8627065f2c0b41
Natascha <njenkns@gmail.com>
2022-04-05 20:09:40 -0700

    passed wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
-class Electronics:
-    pass
+from .item import Item
+
+class Electronics(Item):
+    def __init__(self, condition=0):
+        self.category = "Electronics"
+        self.condition = condition 
+    
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
+
+    
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,21 @@
 class Item:
-    def __init__(self, category=""): # str are immutable, so okay to use as default parameter
+    def __init__(self, category="", condition=0): # str are immutable, so okay to use as default parameter
         self.category = category
+        self.condition = condition 
     
     def __str__(self): 
         return "Hello World!" 
+
+    def condition_description(self):
+        if self.condition <= 0:
+            return "Stay away - it ain't worth it."
+        elif self.condition >= 1 and self.condition < 2:
+            return "You might need some holy water to cleanse it."
+        elif self.condition >= 2 and self.condition < 3:
+            return "It's not bad, but it's not good either."
+        elif self.condition >= 3 and self.condition < 4:
+            return "Not too shabby."
+        elif self.condition >= 4 and self.condition < 5:
+            return "Basically brand-new."
+        elif self.condition == 5:
+            return "Treat yo self!"
