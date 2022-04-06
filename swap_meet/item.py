cb0c41498eecdc1bf71cda6eea120ed57110f4ff
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-05 19:26:42 -0700

    updated for wave 03 stringify.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,10 @@
+
+
 class Item:
-    pass
+    def __init__(self, category=None):
+        if not category:
+            category = ""
+        self.category = category
+
+    def __str__(self):
+        return "Hello World!"
