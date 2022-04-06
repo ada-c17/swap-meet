5d9c84fec19b3fab8eb9801ba2baccf5e1d763ef
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-05 19:53:40 -0700

    updated for wave 05 item condition

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,23 @@
-
-
 class Item:
-    def __init__(self, category=None):
+    def __init__(self, category=None, condition = 0):
         if not category:
             category = ""
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_descriptions (self):
+        if self.condition == 0:
+            return f"terrible"
+        if self.condition == 1:
+            return f"not great"
+        if self.condition == 2:
+            return f"kinda ok"
+        if self.condition == 3:
+            return f"basic"
+        if self.condition == 4:
+            return f"you'll do"
+        if self.condition == 5:
+            return f"vibe"
