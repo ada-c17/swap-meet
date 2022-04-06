2041020eda7d8c6512c9872c2cde04913506c8f7
Alexa Coffman <coffman.alexa@gmail.com>
2022-04-06 16:17:50 -0700

    created clothing, electronics, and decor child classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,11 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, condition = None):
+        super().__init__(condition)
+        self.category = "Clothing"
+
+    def __str__(self):
+        return "The finest clothing you could wear."
+
+    
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,9 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self, condition = None):
+        super().__init__(condition)
+        self.category = "Decor"
+
+    def __str__(self):
+        return "Something to decorate your space."
