f915ed9f768b14b681e98abf00338ac390d78215
Poppy Meng <qim17@pitt.edu>
2022-04-06 14:15:22 -0700

    pass all tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,23 @@
 class Item:
-    pass
+    def __init__(self, category=None, condition=0):
+        if category:
+            self.category=category
+        else:
+            self.category = ""
+        
+        self.condition=condition
+
+    def __str__(self):
+        return "Hello World!"
+    def condition_description(self):
+        if self.condition < 1:
+            return "This item might have some defects"
+        elif 1 <= self.condition < 2:
+            return "This item is in fair condition"
+        elif 2 <= self.condition < 3:
+            return "This item is in good condition"
+        elif 3 <= self.condition < 4:
+            return "This item is in very good condition"
+        elif 4 <= self.condition <= 5:
+            return "This item is in excellent condition"
+        
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,74 @@
+from .item import Item
+
+
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category_name):
+        res = []
+        for item in self.inventory:
+            if item.category == category_name:
+                res.append(item)
+        return res
+
+    def swap_items(self, another_vendor, my_item, their_item):
+        
+        if my_item in self.inventory and their_item in another_vendor.inventory:
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
+            another_vendor.inventory.append(my_item)
+            another_vendor.inventory.remove(their_item)
+            return True
+        return False
+
+    def swap_first_item(self, another_vendor):
+        
+        if not self.inventory or not another_vendor.inventory:
+            return False
+        # my_item = self.inventory.pop(0)
+        # their_item = another_vendor.pop(0)
+        # self.inventory.append(their_item)
+        # another_vendor.inventory.append(my_item)
+        my_item = self.inventory[0]
+        their_item = another_vendor.inventory[0]
+        self.swap_items(another_vendor, my_item, their_item)
+        return True
+
+    def get_best_by_category(self, target_category):
+        if not self.inventory:
+            return None
+        flag = False
+        counter = -1
+        for item in self.inventory:
+            if item.category == target_category:
+                flag = True
+                if item.condition > counter:  # return the first item if there is a tie
+                    res = item
+                    counter=item.condition
+        if not flag:
+            return None
+        return res
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        
+        my_item = self.get_best_by_category(their_priority)
+        their_item = other.get_best_by_category(my_priority)
+        if not my_item or not their_item:
+            return False
+        self.swap_items(other, my_item, their_item)
+        return True
