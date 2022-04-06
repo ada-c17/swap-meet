f7d2a11ee8facf95a615f0c20f9ea3fa03fb3b84
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-04-06 14:53:37 -0700

    wrote Vendor method swap_first_item and passed all tests in wave 04

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,14 +23,13 @@ class Vendor:
         return category_items
 
     def swap_items(self, swap_vendor, my_item, their_item):
-
         """
-        Instances of Vendor will have this method swap_items:
-        * takes 3 arguments; another Vendor, my_item, their_item
-        * removes my_item from this Vendor's inventory
-        * removes their_item from the other Vendor's inventory
-        * returns True if both inventories have the respective items
-        * returns False if one of the items is not contained in respective inventories
+        All instances of Vendor will have this method swap_items:
+        - takes 3 arguments; another Vendor, my_item, their_item
+        - removes my_item from this Vendor's inventory
+        - removes their_item from the other Vendor's inventory
+        - returns True if both inventories have the respective items
+        - returns False if one of the items is not contained in respective inventories
         """
 
         if my_item in self.inventory and their_item in swap_vendor.inventory:
@@ -38,5 +37,24 @@ class Vendor:
             swap_vendor.inventory.append(my_item)
             self.inventory.append(their_item)
             swap_vendor.inventory.remove(their_item)
+            return True
+        return False
+
+    def swap_first_item(self, swap_vendor):
+        """
+        All instances of Vendor will have this method swap_first_items:
+        - takes one argument; another Vendor
+        - method considers the first item in the instance's inventory, and the first item in the friend's inventory
+        - removes the first item from its inventory, and adds the friend's first item
+        - removes the first item from the friend's inventory, and adds the instances first item
+        - returns True
+        - either itself or the friend have an empty inventory, the method returns False
+        """
+        while len(self.inventory) >= 1 and len(swap_vendor.inventory) >= 1:
+            self_first_item = self.inventory[0]
+            swap_vendor_first_item = swap_vendor.inventory[0]
+
+            self.inventory[0] = swap_vendor_first_item
+            swap_vendor.inventory[0] = self_first_item
             return True
         return False
