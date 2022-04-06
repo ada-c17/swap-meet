a722c747d0f86618e96d3521e74d929bb3dbb255
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-06 16:49:07 -0700

    Implement inheritance

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,31 +1,18 @@
-class Clothing:
-    def __init__(self, category="Clothing", condition=0):
-        self.category = category
-        self.condition = condition
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, category = "Clothing", condition = 0):
+        super().__init__(category, condition) # new line
 
     def __str__(self):
         return "The finest clothing you could wear."
 
-    def condition_description(self):
-        if self.condition < 2:
-            return "Quite used but super cute and could refactor it like bad code"
-        elif 2 <= self.condition <= 3:
-            return "Very used but quality fabric and fit"
-        elif 3 <= self.condition <= 4:
-            return "Very used but quality fabric and fit"
-        elif 4 <= self.condition <= 4.5:
-            return "Mint condition, sparkly and all"
-        elif 4.5 <= self.condition <= 4.9:
-            return "Label still on, never worn, like new"
-        elif self.condition == 5:
-            return "Label still on, never worn, like new"
-
 
 
 '''
-Is Clothing a class of category? How is it connected to everything else?
-
-
+A better name for Clothing would have been ClothingItem.
+Which HAS a category Clothing.
+Clothing is a class, and it is a Child of class Item. Item is the Parent class.
 '''
 
 cloth = Clothing()
