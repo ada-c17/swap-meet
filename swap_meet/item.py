dc0769fcd8106d9d5b39582cbd9b305adfa5ab77
Lindsey Still <lindseymstill@gmail.com>
2022-04-07 03:27:01 +0900

    updated condition description message

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -12,4 +12,16 @@ class Item:
         return rep
 
     def condition_description(self):
-        return f"The item's condition is {self.condition} out of 5."
+        if self.condition < 1:
+            custom_message =  "good as garbage"
+        elif 1 <= self.condition < 2:
+            custom_message = "heavily used"
+        elif 2 <= self.condition < 3:
+            custom_message = "moderately used"
+        elif 3 <= self.condition < 4:
+            custom_message = "lightly used"
+        elif 4 <= self.condition < 5:
+            custom_message = "like new"
+        elif self.condition == 5:
+            custom_message = "new"
+        return f"This item is {custom_message}."
