da350497058cebe7714bf70087be0b73d9f45999
Hillary Smith <hillaryhsmith@gmail.com>
2022-04-06 16:40:58 -0700

    passed waves 3 and 4

---
+++ main.py
@@ -0,0 +1,2 @@
+from swap_meet.vendor import Vendor
+from swap_meet.item import Item 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,8 @@
 
 class Vendor:
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
+        if not inventory:
+            inventory = []
         self.inventory = inventory
 
     def add(self, item):
@@ -20,3 +22,29 @@ class Vendor:
             if item.category == category:
                 items_in_category.append(item)
         return items_in_category
+
+    def swap_items(self, vendor, my_item, their_item):
+        
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        
+        self.add(their_item)
+        vendor.add(my_item)
+        self.remove(my_item)
+        vendor.remove(their_item)
+        return True
+    
+    def swap_first_item(self, vendor):
+
+        if not self.inventory or not vendor.inventory:
+            return False
+
+        self.add(vendor.inventory[0])
+        vendor.add(self.inventory[0])
+        self.remove(self.inventory[0])
+        vendor.remove(vendor.inventory[0])
+        return True
+
+
+        
+    
