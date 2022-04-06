b9299b084d5dcb914f18b3fea019653fb43f4ef2
mit <mit.magh@gmail.com>
2022-04-05 22:48:00 -0700

    defined swap_item method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
 class Item:
-    pass
+    def __init__(self, category = None):
+        if category == None:
+            self.category = ""
+        else:
+            self.category = category
+
+    def __str__(self):
+        self.category = "Hello World!" 
+        return self.category
+# item = Item("stringified_item")
+# str(item)
+
+    
+
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,14 @@
+from re import A
+from swap_meet.item import Item
+
+
 class Vendor:
-    def __init__(self, inventory = []):
-        self.inventory = inventory
+    def __init__(self, inventory = None):
+        if inventory == None:
+            self.inventory =[]
+        else:
+            self.inventory = inventory
         
-    
     def add(self, new_item):
         self.inventory.append(new_item)
         return new_item
@@ -13,3 +19,28 @@ class Vendor:
             return item
         else:
             return False
+
+    def get_by_category(self, category):
+        items=[]
+        for item in self.inventory:
+            if str(item.category) == category:
+                items.append(item)
+        return items
+
+    def swap_items(self, other_vendor, my_item, their_item):
+
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:    
+            return False
+        
+        other_vendor.inventory.append(my_item)
+        self.inventory.append(their_item)
+        self.inventory.remove(my_item)
+        other_vendor.inventory.remove(their_item)
+        return True
+            
+            
+
+        
+
+
+        
