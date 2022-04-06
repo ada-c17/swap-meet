349d2fcd5108b3ac086f04b1a442b29615f9d54f
Nina <ninagetsbored@gmail.com>
2022-04-06 15:16:36 -0700

    created Venor and Item classes and passed test_wave_01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return self.inventory[-1]
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            return self.inventory.pop(self.inventory.index(item))
+
+    def get_by_category(self):
+        pass
