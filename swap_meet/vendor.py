a7512da93a61a02271da60ce53c69186292a5555
Shayla Logan <shayla.logan709@gmail.com>
2022-04-06 13:01:38 -0700

    Refactored and added comments

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,5 @@
 from types import NoneType
+from operator import attrgetter
 
 
 class Vendor:
@@ -6,10 +7,13 @@ class Vendor:
         self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
+        #adds item to inventory
         self.inventory.append(item)
         return item
 
     def remove(self, item):
+        # removes item from inventory if it's already there
+        # if not, return false
         if item in self.inventory:
             self.inventory.remove(item)
             return item
@@ -17,6 +21,8 @@ class Vendor:
             return False
 
     def get_by_category(self, category):
+        # isolates all the items matching a specific category
+        # and returns them in the form of a list
         self.category = category
         item_list = []
         for cat in self.inventory:
@@ -26,23 +32,27 @@ class Vendor:
         return item_list
 
     def get_best_by_category(self, category):
-        self.category = category
-        best_condition = 0.0
-        best_item = ""
-        cat_found = False
+        # determine which item is in the best condition
+        # for a given category
+        best_item = None
+        best_cat = 0
 
-        for cat in self.inventory:
-            if cat.category == self.category and cat.condition >= best_condition:
-                cat_found = True
-                best_condition = cat.condition
-                best_item = cat
+        # utilizing the get_by_category function to 
+        # isolate the category
+        best_cat = self.get_by_category(category)
+        # if the function doesn't return a blank list
+        if best_cat:
+            # retreive the best condition
 
-        if cat_found:
-            return best_item
-        else:
-            return None
+            # the attrgetter allows me to just evaluate the condition
+            # within the given instance
+            best_item = max(best_cat, key=attrgetter("condition"))
+        
+        return best_item
 
     def swap_items(self, vendor2, my_item, their_item):
+        # takes an item from first list and puts in it second
+        # takes an item from second list and puts it in first
         self.vendor2 = vendor2
         self.my_item = my_item
         self.their_item = their_item
@@ -57,7 +67,9 @@ class Vendor:
             return True
 
     def swap_first_item(self, friend):
-        if not len(self.inventory) >= 1 or not len(friend.inventory) >= 1:
+        # if neither list is empty then 
+        # perform swap of first item in each list
+        if not self.inventory or not friend.inventory:
             return False
         else:
             my_item = self.inventory[0]
@@ -67,9 +79,12 @@ class Vendor:
             return True
 
     def swap_best_by_category(self, other, my_priority, their_priority):
+        # isolate each vendor's desired category
         their_item = other.get_best_by_category(my_priority)
         my_item = self.get_best_by_category(their_priority)
 
+        # if desired categories are found in the other person's list
+        # then swap
         if not my_item or not their_item:
             return False
         else:
