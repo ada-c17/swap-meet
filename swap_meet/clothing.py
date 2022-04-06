06703d3b2407e204d3c1fc42ca3c795bf3e3612c
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-05 22:02:50 -0700

    Created Clothing class - Wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,8 @@
 class Clothing:
-    pass
+    
+    def __init__(self, category="Clothing", condition=0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "The finest clothing you could wear."
