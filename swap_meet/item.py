7bc1df07c41c17351e961141774550fd7aefb49f
Danielle <dwhyte1226@gmail.com>
2022-04-06 13:48:11 -0500

    added condition description method to Item. Passed unit test

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,12 @@
 class Item:
+
     def __init__(self, category = "", condition = 0.0):
         self.category = category
-        self.condition = condition
-    
+        self.condition = condition #add check that condition is within 0 and 5
+
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        CONDITION_DESCRIPTIONS = {0:"Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five"}
+        return CONDITION_DESCRIPTIONS[int(self.condition)]
