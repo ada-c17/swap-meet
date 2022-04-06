b80435300fe8fb20bcfe2011452b6cee679c8f86
Gabriela Webb <gabw813@gmail.com>
2022-04-05 23:35:25 -0700

    passed wave 5 test 1

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,10 @@
-class Clothing:
-    pass
+# wave 5
+# inherit from item class
+from swap_meet.item import Item
+class Clothing(Item):
+    def __init__(self, category="Clothing"):
+        super().__init__(category)
+        # self.category = category
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
