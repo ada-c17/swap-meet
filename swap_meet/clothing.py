fe56ca47894a8aac6a2d5715c20ff490d9c84329
Danielle <dwhyte1226@gmail.com>
2022-04-06 12:17:25 -0500

    added str method for Clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -4,6 +4,8 @@ class Clothing(Item):
 
     def __init__(self, category = "Clothing"):
         self.category = category
-    
+
+    def __str__(self):
+        return "The finest clothing you could wear."
 
 
