86837da6141e4f505725210d618b8dbbfd3c6908
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-04-05 18:17:46 -0700

    Created class Decor, overrode parent constructor method and string method

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,15 @@
-class Decor:
-    pass
+from .item import Item
+
+class Decor(Item):
+    def __init__(self, category = "Decor", condition = 0.0):
+        '''
+        Constructs attributes for Decor object, default values: category = "Decor", condition =0.
+        '''
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        '''
+        Overrides parent str method and returns message.
+        '''
+        return "Something to decorate your space."
