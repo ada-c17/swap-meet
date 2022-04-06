af6dd0433b0b6434a7d0da2a97f567123c2b3b11
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-05 22:59:28 -0700

    Attempting Decor condition_description Wave 5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -6,3 +6,11 @@ class Decor:
 
     def __str__(self):
         return "Something to decorate your space."
+    
+    def condition_description(self):
+    # range 0 - 1.5 --> "Don't touch with bare hands"
+    # range 1.5 - 2.5 --> "Is it worth it? Probably not."
+    # range 2.5 - 3.5 --> "Meh, it's neutral."
+    # range 3.5 - 4.5 --> "It's a goooood find."
+    # range 4.5 - 5 --> "Top tier item! Must have."
+        pass
