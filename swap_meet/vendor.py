7537125fdfe398dbd26d88773b0e999c2064965e
Danielle <dwhyte1226@gmail.com>
2022-04-06 00:38:54 -0500

    created swap_item method for Vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,16 @@ class Vendor:
     def get_by_category(self, category):
         items_with_category = [item for item in self.inventory if item.category == category]
         return items_with_category
+    
+    def swap_items(self, vendor, item1, item2):
+        try: 
+            index_item1 = self.inventory.index(item1) #check for items in respective vendors inventory
+            index_item2 = vendor.inventory.index(item2)
+            
+        except ValueError:
+            print("Item not found")
+            return False
+        else:
+            self.inventory[index_item1] = item2
+            vendor.inventory[index_item2] = item1
+            return True
