db9ca920ee7960e5e02e402967634ad03bb53b6e
Hope Olaide <hopeolaide@gmail.com>
2022-04-05 20:09:41 -0400

    "Waves 1 and 2 complete."

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+from sre_constants import CATEGORY
+from unicodedata import category
+
+
+
+
 class Item:
-    pass
+    def __init__(self, category = None):
+        if not category:
+            self.category = ""
+        else:
+            self.category = category
+
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,40 @@
+
+from swap_meet.item import Item
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if not inventory:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+    
+    
+    def add(self, item):
+        self.inventory.append(item)
+
+        # print(f"This is the updated inventory: {updated_list}")
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+  
+        return item
+
+    def get_by_category(self, category):
+        if not category:
+            return False
+
+        category_list = []
+        for item in self.inventory:
+            if item.category == category:
+
+                category_list.append(item)
+            
+        return category_list
+
+vendor_2 = Vendor("Vendor_2")
+
+    def swap_items(self, ):
+        
