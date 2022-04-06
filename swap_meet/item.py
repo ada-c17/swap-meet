b2ea820671668eb44f31231ac2e8e31c0709ce46
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-06 12:11:39 -0700

    Implement new attributes and method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,15 +1,30 @@
 #from swap_meet.vendor import Vendor
 
 class Item:
-    def __init__(self, anyString="Hello World!", category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
-        self.y = anyString
-    # def __str__()
+        self.condition = condition
 
     def __str__(self):
-        return str(self.y)
+        return "Hello World!"
+
+    def condition_description(self):
+        if self.condition < 2:
+            return "Quite used but super cute and could refactor it like bad code"
+        elif 2 <= self.condition <= 3:
+            return "Very used but quality fabric and fit"
+        elif 3 <= self.condition <= 4:
+            return "Very used but quality fabric and fit"
+        elif 4 <= self.condition <= 4.5:
+            return "Mint condition, sparkly and all"
+        elif 4.5 <= self.condition <= 4.9:
+            return "Label still on, never worn, like new"
+        elif self.condition == 5:
+            return "Label still on, never worn, like new"
+
+
 
 # item1 = Item()
-# print(str(item1))
-
+# item1.condition = 3.5
+# print(item1.condition_description())
 
