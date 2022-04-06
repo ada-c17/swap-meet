df0dba1c2c8dbf88b6bad070ffca633078b536a9
Sreymompech <sanchhen2013@gmail.com>
2022-04-06 15:27:36 -0700

    In Electronics class, refactoring __init__() function

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,10 +3,9 @@ class Electronics(Item):
     """Child class that inherits attributes and methods from Item class as parent class:"""
     
 
-    def __init__(self, **kwargs):
-        """Overridig the category attribute from Item class by setting default"""
-        super().__init__(**kwargs)
-        self.category = "Electronics"
+    def __init__(self, condition = None):
+        """Setting category as default with "Electronics" which inherits attributes from Item class."""
+        super().__init__(category= "Electronics", condition = condition)
 
 
     def __str__(self):
