9404a54f041dda9266c9a905d6ee30adc4630ba0
Shayla Logan <shayla.logan709@gmail.com>
2022-04-06 11:17:30 -0700

    Added functionality for wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,6 @@
+from types import NoneType
+
+
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = inventory if inventory is not None else []
@@ -22,12 +25,29 @@ class Vendor:
 
         return item_list
 
+    def get_best_by_category(self, category):
+        self.category = category
+        best_condition = 0.0
+        best_item = ""
+        cat_found = False
+
+        for cat in self.inventory:
+            if cat.category == self.category and cat.condition >= best_condition:
+                cat_found = True
+                best_condition = cat.condition
+                best_item = cat
+
+        if cat_found:
+            return best_item
+        else:
+            return None
+
     def swap_items(self, vendor2, my_item, their_item):
         self.vendor2 = vendor2
         self.my_item = my_item
         self.their_item = their_item
 
-        if self.my_item not in self.inventory or their_item not in vendor2.inventory:
+        if self.my_item not in self.inventory or self.their_item not in vendor2.inventory:
             return False
         else:
             self.inventory.remove(my_item)
@@ -48,4 +68,14 @@ class Vendor:
             friend.inventory.remove(their_item)
             friend.inventory.append(my_item)
 
+            return True
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        their_item = other.get_best_by_category(my_priority)
+        my_item = self.get_best_by_category(their_priority)
+
+        if not my_item or not their_item:
+            return False
+        else:
+            self.swap_items(other, my_item, their_item)
             return True
