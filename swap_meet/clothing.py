5b9c4eb6a121bbf3dd14157f0f6eb101ea995bdf
Amel <amelntamark@gmail.com>
2022-04-05 18:11:07 -0700

    make Decor subclass

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,7 +2,7 @@ from .item import Item
 
 class Clothing(Item):
     def __init__(self, category="Clothing"):
-        self.category = "Clothing"
+        self.category = category
 
     def __str__(self):
         return "The finest clothing you could wear."
