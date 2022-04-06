1b6ab89f8d0dc6a606eed0e722d461a34a1545b9
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-05 22:01:19 -0700

    Added condition attribute for Item class - Wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,10 @@
 class Item:
 
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         # Each 'Item' will have an attribute named 'category' that is an empty string by default
         self.category = category
-    
-    
+        self.condition = condition
+
     # String-ify 'Item' using 'str()'
     def __str__(self):
         return "Hello World!"
