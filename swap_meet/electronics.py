38f3add7618d2bbfb5b2b6be35f5cb502a1551ce
Gabriela Webb <gabw813@gmail.com>
2022-04-05 23:39:34 -0700

    passed wave 5 test 3

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,10 @@
-class Electronics:
-    pass
+# wave 5
+# inherit from item class
+from swap_meet.item import Item
+class Electronics(Item):
+    def __init__(self, category="Electronics"):
+        super().__init__(category)
+        # self.category = category
+    
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
