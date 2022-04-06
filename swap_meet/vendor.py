347a79205c1c0dd7db3f447b7a3081cd3fd8b756
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-06 16:59:42 -0700

    Cleaned up comments, etc.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,3 @@
-#from swap_meet.item import Item
-
 class Vendor:
 
     def __init__(self, inventory = None):
@@ -22,27 +20,14 @@ class Vendor:
         return item
 
     def get_by_category(self, category):
-        #list_of_items_per_cat = []  # do i need .self? i do not.
-
-        # for item in self.inventory:
-        #     if item.category == category:
-        #        list_of_items_per_cat.append(item)
-
         # list comprehension
         list_of_items_per_cat = [item for item in self.inventory if item.category == category]
-
         return list_of_items_per_cat
 
     def swap_items(self, other_vendor, my_item, their_item):
-
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
 
-        '''
-        order does not matter
-        self.inventory.remove or other_vendor.remove does not matter
-        self.other_vendor / self.my_item, etc. does not matter
-        '''
         other_vendor.add(my_item)
         self.remove(my_item)
         self.add(their_item)
@@ -51,7 +36,6 @@ class Vendor:
         return True
 
     def swap_first_item(self, other_vendor):
-
         if len(self.inventory) == 0 or len(other_vendor.inventory) == 0:
             return False
 
@@ -63,7 +47,6 @@ class Vendor:
         return True
 
     def get_best_by_category(self, category):
-
         items_by_cat = self.get_by_category(category)
 
         if len(items_by_cat) == 0:
@@ -73,6 +56,7 @@ class Vendor:
         for item in items_by_cat:
             if item.condition > best_item.condition:
                 best_item = item
+
         return best_item
 
     def swap_best_by_category(self, other, my_priority, their_priority):
@@ -85,27 +69,3 @@ class Vendor:
         self.swap_items(other, best_item_mine, best_item_other)
 
         return True
-
-
-rafferty = Vendor()
-closet_items = ["white shirt", "balenciaga jeans", "flat shoes", "work gown"]
-rafferty.inventory = closet_items
-# print(vars(rafferty))
-rafferty.add("elegant work gown")
-rafferty.remove("flat shoes")
-# print(vars(rafferty))
-oriana = Vendor()
-closet_items2 = ["yellow shirt", "method jeans", "ballet flats", "red jacket"]
-oriana.inventory = closet_items2
-print(vars(rafferty))
-rafferty.swap_items(oriana, rafferty.inventory[0], oriana.inventory[0])
-print(vars(rafferty))
-
-
-'''
-Notes and Questions:
-
-
-# questions
-
-'''
