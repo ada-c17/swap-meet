0744a5891def5783d6cd325d20a197c4499cbbe5
Gabriela Webb <gabw813@gmail.com>
2022-04-05 23:37:41 -0700

    passed wave 5 test 2

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,10 @@
-class Decor:
-    pass
+# wave 5
+# inherit from item class
+from swap_meet.item import Item
+class Decor(Item):
+    def __init__(self, category="Decor"):
+        super().__init__(category)
+        # self.category = category
+    
+    def __str__(self):
+        return "Something to decorate your space."
--- tests/unit_tests/test_wave_05.py
+++ tests/unit_tests/test_wave_05.py
@@ -9,7 +9,7 @@ def test_clothing_has_default_category_and_to_str():
     assert cloth.category == "Clothing"
     assert str(cloth) == "The finest clothing you could wear."
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_decor_has_default_category_and_to_str():
     decor = Decor()
     assert decor.category == "Decor"
