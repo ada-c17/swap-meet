b1c67cdcf85eb4688ce5c0638a297e4c23dc9a26
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-05 18:24:51 -0700

    Passing Test 1 Wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,13 @@
 class Item:
 
-    def __init__(self, category=''):
+    def __init__(self, category=""):
         # Each 'Item' will have an attribute named 'category' that is an empty string by default
         self.category = category
+    
+    
+    # String-ify 'Item' using 'str()'
+    def __str__(self):
+        return "Hello World!"
+
+
+    # Write the method 'swap_items'
