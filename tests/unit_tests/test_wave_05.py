eefe346a93c33940f43c5c5dece91470669f18fc
Sreymompech <sanchhen2013@gmail.com>
2022-04-06 16:06:13 -0700

    In test_wave_05.py, adding addtional test cases on function condition_description() to complete the code coverage

--- tests/unit_tests/test_wave_05.py
+++ tests/unit_tests/test_wave_05.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
@@ -52,3 +53,37 @@ def test_items_have_condition_descriptions_that_are_the_same_regardless_of_type(
         assert item.condition_description() == one_condition_description
 
     assert one_condition_description != five_condition_description
+
+# Additional test cases on condition_description() function to complete the code coverage
+# @pytest.mark.skip
+def test_condition_description_with_all_conditions_are_0():
+    # Arrange
+    items = [
+        Clothing(condition=0),
+        Decor(condition=0),
+        Electronics(condition=0)
+    ]
+
+    # Act & Assert
+    assert len(items) == 3
+    for i in range(len(items)):
+        assert items[i].condition_description() == "New condition"
+        assert items[i].condition == 0
+
+# @pytest.mark.skip
+def test_condition_description_with_invalid_input():
+    # Arrange
+    items = [
+        Clothing(condition="Good"),
+        Decor(condition="Bad"),
+        Electronics(condition="Fair")
+    ]
+
+    # Act & Assert
+    assert len(items) == 3
+    assert items[0].condition == "Good"
+    assert items[1].condition == "Bad"
+    assert items[2].condition == "Fair"
+    for i in range(len(items)):
+        assert items[i].condition_description() == "Invalid input value!"
+
