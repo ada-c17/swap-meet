74486743468fa28b66b1ef6ed708f93b4835c134
Shayla Logan <shayla.logan709@gmail.com>
2022-04-06 11:16:29 -0700

    Updated __init__ to use super()

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,9 +1,8 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, category = "Electronics", condition = 0):
-        self.category = category
-        self.condition = condition
+    def __init__(self, condition = 0.0):
+        super().__init__(category = "Electronics", condition = condition)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
