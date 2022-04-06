c4d6573e75285a3fd5b0c86def6379edcc574828
Gabriela Webb <gabw813@gmail.com>
2022-04-05 18:55:02 -0700

    passed wave 2 test 1

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
 class Item:
-    pass
+    # wave 2
+    # attributes: 
+    #   category - an empty string
+    # instance methods:
+    #   get_by_category - takes in one string item and returns list of inventory items in the same category
+    def __init__(self, category=""):
+        self.category = category
--- tests/unit_tests/test_wave_02.py
+++ tests/unit_tests/test_wave_02.py
@@ -2,7 +2,7 @@ import pytest
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_items_have_blank_default_category():
     item = Item()
     assert item.category == ""
