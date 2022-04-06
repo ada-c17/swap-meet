742cb539ad61a22218120a543f9f17dfe09b2d9e
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-06 15:08:03 -0500

    Updated Decor class to inherit from Item class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,19 +1,8 @@
-class Decor:
+from swap_meet.item import Item
+
+class Decor(Item):
     def __init__(self, category="Decor", condition=0):
-        self.category = category
-        self.condition = condition
+        super().__init__(category, condition)
     
     def __str__(self):
         return "Something to decorate your space."
-
-    def condition_description(self):
-        CONDITION_RATING = {
-            1: "parts only",
-            2: "play condition",
-            3: "good used condition",
-            4: "excellent used condition",
-            5: "like new condition"
-        }
-
-        condition = CONDITION_RATING[self.condition]
-        return condition
