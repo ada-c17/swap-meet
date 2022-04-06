e41386d681ef5cc20f6c63a38b2308bfd574a516
Danielle <dmbecker24@gmail.com>
2022-04-06 11:35:28 -0700

    Create clothing, electronic and decor child classes. Update item class with condition_description method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,24 @@
+from pydoc import describe
+
+
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, condition = 0, category = ""):
+        self.condition = condition
         self.category = category
 
     def __str__(self): 
         return "Hello World!"
+    
+    def condition_description(self):
+        if 0 <= self.condition < 1:
+            self.description = "Heavily used"
+            return self.description
+        elif 1 <= self.condition < 3:
+            self.description = "Moderately used"
+            return self.description
+        elif 3 <= self.condition < 4:
+            self.description = "Good condition"
+            return self.description
+        elif 4 <= self.condition <= 5:
+            self.description = "Great condition"
+            return self.description
