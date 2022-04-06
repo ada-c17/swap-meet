9581dc828b5fc3ebaa0e83f7a3a38a2ef8b84208
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-05 22:57:11 -0700

    Progress on Wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,3 +8,12 @@ class Item:
     # String-ify 'Item' using 'str()'
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        # range 0 - 1.5 --> "Don't touch with bare hands"
+        # range 1.5 - 2.5 --> "Is it worth it? Probably not."
+        # range 2.5 - 3.5 --> "Meh, it's neutral."
+        # range 3.5 - 4.5 --> "It's a goooood find."
+        # range 4.5 - 5 --> "Top tier item! Must have."
+        
+        pass
