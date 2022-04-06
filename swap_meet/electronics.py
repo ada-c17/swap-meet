dc57897844d88ea0032b953742987039df69e7df
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-06 15:06:49 -0500

    Updated Electronics class to inherit from Item class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,19 +1,8 @@
-class Electronics:
+from swap_meet.item import Item
+
+class Electronics(Item):
     def __init__(self, category="Electronics", condition=0):
-        self.category = category
-        self.condition = condition
+        super().__init__(category, condition)
     
     def __str__(self):
         return "A gadget full of buttons and secrets."
-
-    def condition_description(self):
-        CONDITION_RATING = {
-            1: "parts only",
-            2: "play condition",
-            3: "good used condition",
-            4: "excellent used condition",
-            5: "like new condition"
-        }
-
-        condition = CONDITION_RATING[self.condition]
-        return condition
