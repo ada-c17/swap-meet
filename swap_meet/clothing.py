11e2c19884d4773e9114453f356163c1e69b16eb
Danielle <dwhyte1226@gmail.com>
2022-04-06 13:30:40 -0500

    added condition attribute to Item class and subclasses. Passed united test

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,8 +2,9 @@ from swap_meet.item import Item
 
 class Clothing(Item):
 
-    def __init__(self, category = "Clothing"):
-        self.category = category
+    def __init__(self, condition=0.0):
+        super().__init__(category="Clothing", condition=condition)
+        #self.condition = condition
 
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,9 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, category = "Decor"):
-        self.category = category
+    def __init__(self, condition=0.0):
+        super().__init__(category = "Decor", condition=condition)
+        #self.category = category
 
     def __str__(self):
         return "Something to decorate your space."
