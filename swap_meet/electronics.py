c17cb8726844e15f3792b9428bc2a6816fcb8d26
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-05 22:01:47 -0700

    Created Electronics class - Wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
 class Electronics:
-    pass
+    
+    def __init__(self, category="Electronics", condition=0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
