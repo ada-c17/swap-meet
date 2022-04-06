ea1b3bb3dd7748f6453cde0db36c8cbd0a234926
Lindsey Still <lindseymstill@gmail.com>
2022-04-06 09:43:51 +0900

    constructed clothing class and updated __str__ method

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,6 @@
 class Clothing:
-    pass
+    def __init__(self, category = "Clothing"):
+        self.category = category
+
+    def __str__(self):
+        return "The finest clothing you could wear."
