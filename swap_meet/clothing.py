1353038eeb645c8c712e359178dcaa3db1ca0456
Sreymompech <sanchhen2013@gmail.com>
2022-04-05 23:11:34 -0700

    In Clothing class, refactoring function __str__(), deleting condition_description(), and updating the doc string

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,18 +2,16 @@ from swap_meet.item import Item
 class Clothing(Item):
     """Child class that inherits attributes and methods from Item class as parent class:"""
 
-    # Wave 5
-    def __init__(self, category = "Clothing", **kargs):
-        """Inheriting all attributs from Item class"""
-        super().__init__(category = "Clothing", **kargs)
+
+    def __init__(self, **kwargs):
+        """Overridig the category attribute from Item class by setting default"""
+        super().__init__(**kwargs)
+        self.category = "Clothing"
 
 
     def __str__(self):
-        """Returning value in string format by override inherited method from Item class."""
-        return str("The finest clothing you could wear.")
+        """Overriding method from Item class by returning differenct value in string format if condition is empty."""
+        if not self.condition:
+            return "The finest clothing you could wear."
 
-
-    def condition_description(self):
-        """Returing inherited method from Item class."""
-        return super().condition_description()
     
