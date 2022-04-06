0632268795028dface2ccc6cefb74a6d687ae873
Thu Vuong <tavuongg@gmail.com>
2022-04-06 14:15:32 -0700

    added spaces between functions

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -4,5 +4,6 @@ class Decor(Item):
     def __init__(self, condition=0):
         super().__init__(category="Decor", condition=condition)
 
+
     def __str__(self):
         return "Something to decorate your space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -4,5 +4,6 @@ class Electronics(Item):
     def __init__(self, condition=0):
         super().__init__(category="Electronics", condition=condition)
 
+
     def __str__(self):
         return "A gadget full of buttons and secrets."
