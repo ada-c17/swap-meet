2f29c6c1ea2f4dcf1158dffdb6d833a75b7c7656
verasazonova <vera.zhokhova@gmail.com>
2022-04-05 20:09:16 -0700

    Done with wave 5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,9 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self, category = "Decor", condition = 0):
+        self.category = category
+        self.condition = condition
+    
+    def __str__(self):
+        return "Something to decorate your space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,14 +6,30 @@ class Item:
         self.category = category
         self.condition = condition    
 
+
     # return string, when instance is assign
     def __repr__(self):
         return self.category
 
-    # return string "Hello World!"
+
+    # return string "Hello World!" if category is not defined
     def __str__(self):
         if not self.category:
             return "Hello World!"
         return self.category
 
-    # def condition_description(self):
+
+    def condition_description(self):
+        
+        if 0 < self.condition <= 1:
+            condition_description = "Poor"
+        elif 1 < self.condition <= 2:
+            condition_description = "Fair"
+        elif 2 < self.condition <= 3:
+            condition_description = "Good"
+        elif 3 < self.condition <= 4:
+            condition_description = "Like New"
+        elif 4 < self.condition <= 5:
+            condition_description = "New"
+
+        return condition_description
