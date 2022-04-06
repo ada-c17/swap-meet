bde57cc11c3b6d2b46a618cb1f12e9caa7bfca6c
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-06 16:50:22 -0700

    Implement inheritance

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,21 +1,8 @@
-class Electronics:
-    def __init__(self, category="Electronics", condition=0):
-        self.category = category
-        self.condition = condition
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category = "Electronics", condition = 0):
+        super().__init__(category, condition)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
-
-    def condition_description(self):
-        if self.condition < 2:
-            return "Quite used but super cute and could refactor it like bad code"
-        elif 2 <= self.condition <= 3:
-            return "Very used but quality fabric and fit"
-        elif 3 <= self.condition <= 4:
-            return "Very used but quality fabric and fit"
-        elif 4 <= self.condition <= 4.5:
-            return "Mint condition, sparkly and all"
-        elif 4.5 <= self.condition <= 4.9:
-            return "Label still on, never worn, like new"
-        elif self.condition == 5:
-            return "Label still on, never worn, like new"
