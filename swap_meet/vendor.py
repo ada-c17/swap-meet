c91fee9bb32c002bfdabd01f34910fabf1be8ee6
Gabriela Webb <gabw813@gmail.com>
2022-04-05 19:09:06 -0700

    passed wave 2 test 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -6,6 +6,10 @@ class Vendor:
     #   add - takes in one item and adds to inventory, returns item added
     #   remove - takes in one item and removes from inventory, returns item removed
 
+    # wave 2
+    # add instance method:
+    #   get_by_category - takes in one string item and returns list of inventory items in the same category
+
     def __init__(self, inventory=[]):
         self.inventory = inventory
     
@@ -18,3 +22,10 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+
+    def get_by_category(self, category):
+        same_category_items = []
+        for item in self.inventory:
+            if item.category == category:
+                same_category_items.append(item)
+        return same_category_items
