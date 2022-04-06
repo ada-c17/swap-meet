65e8e70bcfb962534479abdaf2fbf7759692edac
mit <mit.magh@gmail.com>
2022-04-05 23:04:26 -0700

    end of wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,17 +27,25 @@ class Vendor:
                 items.append(item)
         return items
 
-    def swap_items(self, other_vendor, my_item, their_item):
+    def swap_items(self, another_vendor, my_item, their_item):
 
-        if my_item not in self.inventory or their_item not in other_vendor.inventory:    
+        if my_item not in self.inventory or their_item not in another_vendor.inventory:    
             return False
         
-        other_vendor.inventory.append(my_item)
+        another_vendor.inventory.append(my_item)
         self.inventory.append(their_item)
         self.inventory.remove(my_item)
-        other_vendor.inventory.remove(their_item)
+        another_vendor.inventory.remove(their_item)
         return True
             
+    def swap_first_item(self, friend):
+        if self.inventory == [] or friend.inventory == []:
+            return False
+        self.inventory.append(friend.inventory[0])
+        friend.inventory.append(self.inventory[0])
+        self.inventory.remove(self.inventory[0])
+        friend.inventory.remove(friend.inventory[0])
+        return True
     
             
 
