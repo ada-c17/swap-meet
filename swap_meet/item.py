7f7ff845476a30602ab75a730e70577f5df816d5
MandyG415 <missmandygao@gmail.com>
2022-04-05 17:39:19 -0700

    Finished with Wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,6 @@
-class Item:
-    pass
+class Item():
+  def __init__ (self, category = ""):
+    self.category = category
+
+  def __str__(self):
+    return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,32 @@
-class Vendor:
-    pass
+class Vendor():
+  def __init__ (self, inventory = None):
+    if inventory is None: #do not want mutable object
+      inventory = [] #as a parameter, so I set to None
+    self.inventory = inventory #empty list
+  
+  def add(self, item):
+    self.inventory.append(item)
+    return item
+
+  def remove(self, item):
+    if item in self.inventory:
+      self.inventory.remove(item)
+      return item
+    else:
+      return False
+  def get_by_category(self,string_category):
+    inventory_same_category = []
+    for item in self.inventory:
+      if item.category == string_category: #we want category here
+        inventory_same_category.append(item) #only append item associated with category
+    return inventory_same_category#list of inventory
+  
+  def swap_items(self, vendor, my_item, their_item):
+    if my_item in self.inventory and their_item in vendor.inventory:
+      self.inventory.remove(my_item)
+      vendor.inventory.append(my_item) #add to fatimah inventory
+      vendor.inventory.remove(their_item)
+      self.inventory.append(their_item)
+      return True
+    else:
+      return False
