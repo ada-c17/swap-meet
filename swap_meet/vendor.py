1a924fd2b77a99a05e71ffd9ffffe7b9ae826a01
Gabriela Webb <gabw813@gmail.com>
2022-04-05 20:38:35 -0700

    passed wave 3 tests 2-6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -10,6 +10,15 @@ class Vendor:
     # add instance method:
     #   get_by_category - takes in one string item and returns list of inventory items in the same category
 
+    # wave 3
+        # add instance method:
+        # swap_items
+            # args: Vendor, Item(my_item), Item(their_item)
+            # removes my_item from vendor's inventory and adds to friend's inventory
+            # removes their_item from friend's inventory and adds to vendor inventory
+            # returns True
+            # returns False if my_item not in Vendor's inventory or their_item not in freind's inventory
+
     def __init__(self, inventory=[]):
         self.inventory = inventory
     
@@ -29,3 +38,13 @@ class Vendor:
             if item.category == category:
                 same_category_items.append(item)
         return same_category_items
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        else:
+            self.inventory.remove(my_item)
+            vendor.inventory.append(my_item)
+            vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            return True
