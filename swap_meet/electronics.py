98c0564d2f80f6a4d25d428343664875640aa248
Danielle <dwhyte1226@gmail.com>
2022-04-06 12:39:56 -0500

    created init and str methods for Electronics, passed unit test

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category = "Electronics"):
+        self.category = category
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
