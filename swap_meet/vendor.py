af510a3f3df4bfb3ffe6e2547a9937c68e9ced52
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-04-06 14:05:31 -0700

    use __str__ method to make Item return the default str 'Hello World!', create Vendor method swap_item, and passed all tests in wave 03... refactored swap_items from nested if statements for Vendor and other Vendor to one if statement using 'and'

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from swap_meet.item import Item
+
 class Vendor:
 
     def __init__(self, inventory = []):
@@ -19,3 +21,32 @@ class Vendor:
             if item.category == category_str:
                 category_items.append(item)
         return category_items
+
+    def swap_items(self, swap_vendor, my_item, their_item):
+
+        """
+        Instances of Vendor will have this method swap_items:
+        * takes 3 arguments; another Vendor, my_item, their_item
+        * removes my_item from this Vendor's inventory
+        * removes their_item from the other Vendor's inventory
+        * returns True if both inventories have the respective items
+        * returns False if one of the items is not contained in respective inventories
+        """
+
+        if my_item in self.inventory and their_item in swap_vendor.inventory:
+            self.inventory.remove(my_item)
+            swap_vendor.inventory.append(my_item)
+            self.inventory.append(their_item)
+            swap_vendor.inventory.remove(their_item)
+            return True
+            
+        return False
+
+        # if my_item in self.inventory:
+        #     self.inventory.remove(my_item)
+        #     swap_vendor.inventory.append(my_item)
+        #     if their_item in swap_vendor.inventory:
+        #         self.inventory.append(their_item)
+        #         swap_vendor.inventory.remove(their_item)
+        #         return True
+        # return False
