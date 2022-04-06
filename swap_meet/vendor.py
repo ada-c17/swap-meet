4d1f8ff8f73bcacdd91624ba04432278b8fc9ad7
Uyen Vong <uvong92@gmail.com>
2022-04-05 20:59:28 -0700

    refactors methods

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,46 +22,37 @@ class Vendor:
         return item
 
     def get_by_category(self, category):
-        '''Input: category(string), returns inventory list
-        with items of that category'''
+        '''Input: category(string), returns inventory list with items of that category'''
         return [item for item in self.inventory if item.category == category]
         
     def swap_items(self, other, my_item, their_item):
-        '''
-        Inputs: other Vendor object, this vendor's item object, other vendor's item.
-        Swaps item objects between this vendor and other vendor's inventories 
-        '''  
-        if my_item not in self.inventory or \
-            their_item not in other.inventory:
-            return False
+        '''Inputs: other Vendor object, this vendor's item object, other vendor's item.
+        Swaps item objects between this vendor and other vendor's inventories'''
 
-        # remove() method returns item object 
-        # which we can use as the argument for the add() method     
-        self.add(other.remove(their_item))
-        other.add(self.remove(my_item))
-        
-        return True
+        if my_item in self.inventory and their_item in other.inventory:
+            # item returned from remove() method gets used as the argument for add() method     
+            self.add(other.remove(their_item))
+            other.add(self.remove(my_item))
+            return True
     
     def swap_first_item(self, other):
-        '''Input: other vendor object.
-        Swaps the first items in current and other vendor's inventories'''
-        if not self.inventory or not other.inventory:
-            return False   
-        return self.swap_items(other, self.inventory[0], other.inventory[0])
+        '''Input: other vendor object. Swaps the first items in current and other vendor's inventories'''
+        
+        if self.inventory and other.inventory:  
+            return self.swap_items(other, self.inventory[0], other.inventory[0])
 
     def get_best_by_category(self, category):
-        '''Input: category(string). 
-        Returns an item object with the highest condition in that category'''    
-        if not self.get_by_category(category):
-            return None
+        '''Input: category(string). Returns an item with the highest condition in that category'''    
+        
+        if self.get_by_category(category):
         #sort based on attribute of interest 
-        return max(self.get_by_category(category), key=attrgetter("condition"))
+            return max(self.get_by_category(category), key=attrgetter("condition"))
         
     def swap_best_by_category(self, other, my_priority, their_priority):
         '''Inputs: other vendor object, this object's priority category, 
         other object's priority category.
-        Swaps priority item objects between this vendor and other vendor's inventories
-        '''
+        Swaps priority item objects between this vendor and other vendor's inventories'''
+
         my_best_item = self.get_best_by_category(their_priority)
         their_best_item = other.get_best_by_category(my_priority)
         
@@ -70,9 +61,8 @@ class Vendor:
     def swap_by_newest(self, other):
         '''Input: other vendor object.
         Swaps the newest items in current and other vendor's inventories'''
-        if not self.inventory or not other.inventory:
-            return False
-
-        my_newest = min(self.inventory, key=attrgetter("age"))
-        their_newest = min(other.inventory, key=attrgetter("age"))
-        return self.swap_items(other, my_newest, their_newest)
+        
+        if self.inventory and other.inventory:
+            my_newest = min(self.inventory, key=attrgetter("age"))
+            their_newest = min(other.inventory, key=attrgetter("age"))
+            return self.swap_items(other, my_newest, their_newest)
