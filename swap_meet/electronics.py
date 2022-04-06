a8b6b577b2ac3cce0b8f27b01ef3125dad767739
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-06 12:52:42 -0500

    Updated category attribute in Electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,6 +1,6 @@
 class Electronics:
     def __init__(self, category="Electronics"):
-        self.category = "Electronics"
+        self.category = category
     
     def __str__(self):
         return "A gadget full of buttons and secrets."
