0cf3772b8acccb118b4534e17ac9cf40f1ba9482
Morgan Buntin <mmbuntin@gmail.com>
2022-04-06 17:53:40 -0500

    Made adjustments to pass wave 5. Added inheritence

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,21 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0.0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 0.0:
+            return "Condition unknown or not supplied."
+        elif self.condition > 0.0 and self.condition <= 1.0:
+            return "This should be swapped into the dumpster."
+        elif self.condition > 1.0 and self.condition <= 2.0:
+            return "This item's condition is as dubious as leftovers you forgot about."
+        elif self.condition > 2.0 and self.condition <= 3.0:
+            return "This item is okay, nothing to get excited about."
+        elif self.condition > 3.0 and self.condition <= 4.0:
+            return "This item is in above average condition! Score!"
+        elif self.condition > 4.0:
+            return "This item's condition is suspiciously good. You definitely want it!"
