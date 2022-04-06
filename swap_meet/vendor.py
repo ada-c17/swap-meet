2043a535be3864f150decced99199ee9007a1da3
Morgan Buntin <mmbuntin@gmail.com>
2022-04-06 14:51:09 -0400

    Changed vendor and item classes to pass wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,3 +27,13 @@ class Vendor:
             return category_items
         else:
             return False
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
+            other_vendor.inventory.remove(their_item)
+            other_vendor.inventory.append(my_item)
+            return True
+        else:
+            return False
