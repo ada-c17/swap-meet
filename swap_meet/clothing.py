e48ec28cd13e59a593c79a2aa22bfe62d5462afc
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-04-06 15:09:13 -0500

    Update Clothing class to inherit from Item class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,19 +1,8 @@
-class Clothing:
+from swap_meet.item import Item
+
+class Clothing(Item):
     def __init__(self, category="Clothing", condition=0):
-        self.category = category
-        self.condition = condition
+        super().__init__(category, condition)
     
     def __str__(self):
         return "The finest clothing you could wear."
-
-    def condition_description(self):
-        CONDITION_RATING = {
-            1: "parts only",
-            2: "play condition",
-            3: "good used condition",
-            4: "excellent used condition",
-            5: "like new condition"
-        }
-
-        condition = CONDITION_RATING[self.condition]
-        return condition
