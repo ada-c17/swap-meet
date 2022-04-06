110b272cc87d92fce2a38c7994080305ece52647
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-04-06 12:09:55 -0700

    Oooops forgot to commit updates... updated vendor and item classes, passing all tests in waves 01 and 02, finsihed wave assertions

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,24 @@
+from unicodedata import category
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item 
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+    def get_by_category(self, category_str):
+        category_items = []
+        for item in self.inventory:
+            if item.category == category_str:
+                category_items.append(item)
+        return category_items
