af4bcff9024f2e162cf9d94dc02ea63068464a76
tinghj17 <tinghj17@gmail.com>
2022-04-05 18:03:05 -0700

    Passed wave 05

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,24 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category):
         self.category = category
     
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Distressed"
+        elif self.condition == 1:
+            return "Used fair"
+        elif self.condition == 2:
+            return "Used good"
+        elif self.condition == 3:
+            return "Used very good"
+        elif self.condition == 4:
+            return "Used like new"
+        elif self.condition == 5:
+            return "New"
+            
+
 
 
