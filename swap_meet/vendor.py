2772c4eeae7a322e27b4d05e3ffca97f65fcfa29
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-05 23:02:18 -0700

    Implement methods for wave3, wave4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,15 +26,45 @@ class Vendor:
         return item
 
     def get_by_category(self, category):
-        self.list_of_items_per_cat = []
+        list_of_items_per_cat = []  # do i need .self? i do not.
 
         for item in self.inventory:
             if item.category == category:
-                self.list_of_items_per_cat.append(item)
+               list_of_items_per_cat.append(item)
 
-        print(self.list_of_items_per_cat)
+        #print(list_of_items_per_cat)
 
-        return self.list_of_items_per_cat
+        return list_of_items_per_cat
+
+    def swap_items(self, other_vendor, my_item, their_item):
+
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+
+        '''
+        order does not matter
+        self.inventory.remove or other_vendor.remove does not matter
+        self.other_vendor / self.my_item, etc. does not matter
+        '''
+        other_vendor.add(my_item)
+        self.remove(my_item)
+        self.add(their_item)
+        other_vendor.remove(their_item)
+
+        return True
+
+    def swap_first_item(self, other_vendor):
+
+        if len(self.inventory) == 0 or len(other_vendor.inventory) == 0:
+            return False
+
+        self.add(other_vendor.inventory[0])
+        other_vendor.add(self.inventory[0])
+        self.remove(self.inventory[0])
+        other_vendor.remove(other_vendor.inventory[0])
+
+        return True
+
 
 
 
