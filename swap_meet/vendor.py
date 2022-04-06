ca186c756a69b67f1a2654cd8b66e3be878e7e7a
Julie <jawarren89@gmail.com>
2022-04-06 14:02:23 -0700

    Created remove method for Vendor class, updated constructor structure

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,17 @@
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
 
     def add(self, item):
         self.inventory.append(item)
         return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
