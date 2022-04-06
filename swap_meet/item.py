6d56ccd227bb5331ee0e1e925360e28fa6968c42
Gabriela Webb <gabw813@gmail.com>
2022-04-05 23:59:32 -0700

    passed wave 5 test 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -7,6 +7,7 @@
 
     # wave 5
     # add condition attribute
+    # add instance method condition_description
 class Item:
     def __init__(self, category="", condition=0):
         self.category = category
@@ -14,3 +15,18 @@ class Item:
     
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            description = "Trash"
+        elif self.condition == 1:
+            description = "It Works"
+        elif self.condition == 2:
+            description = "Decent"
+        elif self.condition == 3:
+            description = "Good"
+        elif self.condition == 4:
+            description = "Like New"
+        elif self.condition == 5:
+            description = "New"
+        return description
