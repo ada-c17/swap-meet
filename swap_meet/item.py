ae22cc50ae4ed2013281570cd4d3881e29541879
Gabriela Webb <gabw813@gmail.com>
2022-04-05 20:19:15 -0700

    passed wave 3 test 1

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,7 +2,9 @@ class Item:
     # wave 2
     # attributes: 
     #   category - an empty string
-    # instance methods:
-    #   get_by_category - takes in one string item and returns list of inventory items in the same category
+
     def __init__(self, category=""):
         self.category = category
+    
+    def __str__(self):
+        return "Hello World!"
