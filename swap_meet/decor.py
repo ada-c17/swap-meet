a1ab73bd2647627f892c7e98d44000e5db506307
Kiana <kianashui@gmail.com>
2022-04-06 11:33:52 -0700

    refactored item category __init__ functions to have default categories

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,7 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, category="", condition=0, age=0):
+    def __init__(self, category="Decor", condition=0, age=0):
         super().__init__(category, condition, age)
-        self.category = "Decor"
     def __str__(self):
         return "Something to decorate your space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,7 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, category="", condition=0, age=0):
+    def __init__(self, category="Electronics", condition=0, age=0):
         super().__init__(category, condition, age)
-        self.category = "Electronics"
     def __str__(self):
         return "A gadget full of buttons and secrets."
