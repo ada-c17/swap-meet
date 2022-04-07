e67a267208df6558529ab2c3f772ccd0cf5be213
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-06 18:40:39 -0700

    Refactor conditional

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -7,15 +7,15 @@ class Item:
         return "Hello World!"
 
     def condition_description(self):
-        if self.condition < 2:
+        if self.condition < 3:
             return "Quite used but super cute and could refactor it like bad code"
-        elif 2 <= self.condition <= 3:
-            return "Very used but quality fabric and fit"
-        elif 3 <= self.condition <= 4:
-            return "Not bad"
-        elif 4 <= self.condition <= 4.5:
+        # elif 1 <= self.condition < 2:
+        #     return "Not bad, not bad at all"
+        # elif 1 <= self.condition < 2:
+        #    return "Very used but quality fabric and fit"
+        # elif 3 <= self.condition < 4:
+        #     return "Passable"
+        elif 3 <= self.condition < 5:
             return "Mint condition, sparkly and all"
-        elif 4.5 <= self.condition <= 4.9:
-            return "Never worn, like new"
         elif self.condition == 5:
-            return "Label still on, never worn, like new"
+            return "Nevern worn, labels and all, not like new -- it IS new!"
