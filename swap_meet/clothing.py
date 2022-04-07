27fa4c030458f40afb9df66ba3a21b26dee19c41
angelica <djyl.ahn@gmail.com>
2022-04-07 12:03:13 -0700

    passes all tests

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,8 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, age=0, condition=0, category="Clothing"):
+        super().__init__(age, condition, category)
+
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, age=0, condition=0, category="Electronics"):
+        super().__init__(age, condition, category)
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
