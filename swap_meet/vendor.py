c7f6331818afd813c98e7f972b6e29e336e599ae
Sreymompech <sanchhen2013@gmail.com>
2022-04-07 01:35:08 -0700

    In Vendor class, adding two function get_newest_by_category() and swap_newest_by_category() for optional enhancements, and refactored condition on other methods like add, remove, swap_items, and get_best_by_category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -7,7 +7,6 @@ from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
 class Vendor:
     
-
     # Wave 1
     def __init__(self, inventory = None):
         """Inventory is keyword argument that optionally pass in."""
@@ -20,14 +19,16 @@ class Vendor:
         
     def add(self, item):
         """Adding new item into inventory and returns the added item."""
-        self.inventory.append(item)
-        return item
+        # make sure input is not empty before adding
+        if item:
+            self.inventory.append(item)
+            return item
         
-
 
     def remove(self, item):
         """Removing item from inventory if it is matching the paramenter pass in and returns that removed item."""
-        if item in self.inventory:
+        # make sure input is not empty before removing
+        if item and item in self.inventory:
             self.inventory.remove(item)
             return item
         return False
@@ -37,8 +38,8 @@ class Vendor:
     def get_by_category(self, category):
         """Returnning list of items if matching the conditions, otherwise, return []"""
         items_list = []
-        # adding item into the new list if the inventory is not empty and category was the input
-        if len(self.inventory) > 0:
+        # adding item into the new list if the inventory is not empty and matches category
+        if category and len(self.inventory) > 0:
             for item in self.inventory:
                 if item.category == category:
                     items_list.append(item)
@@ -49,14 +50,15 @@ class Vendor:
     # Wave 3
     def swap_items(self, vendor, my_item, their_item):
         """Returning True if item been swapped between both inventories, otherwise, return False"""
-        # make sure both inventories are not empty and both items that want to swap are in both inventories
+        # make all inputs are not empty before swapping
         if len(self.inventory) > 0 and len(vendor.inventory) > 0:
-            if my_item in self.inventory and their_item in vendor.inventory:
-                self.inventory.remove(my_item)
-                self.inventory.append(their_item)
-                vendor.inventory.append(my_item)
-                vendor.inventory.remove(their_item)
-                return True
+            if my_item and my_item in self.inventory:
+                if their_item and their_item in vendor.inventory:
+                    self.inventory.remove(my_item)
+                    self.inventory.append(their_item)
+                    vendor.inventory.append(my_item)
+                    vendor.inventory.remove(their_item)
+                    return True
         return False
             
 
@@ -76,7 +78,7 @@ class Vendor:
         # if inventory and category are not empty then find the highest condition that is matching category for best item
         if category and len(self.inventory) > 0:
             for item in self.inventory:
-                if item.category == category:
+                if (isinstance(item.condition, int) or isinstance(item.condition, float)) and item.category == category :
                     max_condi = max([item.condition for item in self.inventory if item.category == category])
                     if item.condition == max_condi: 
                         return item
@@ -85,13 +87,35 @@ class Vendor:
     
     def swap_best_by_category(self, other, my_priority, their_priority ):
         """Swapping item by best item and returning invoked function swap_items() at the end."""
-        
         # invoking method get_best_by_category() to find my best item and other best item base on priority.
         my_best_item = self.get_best_by_category(their_priority)
         other_best_item = other.get_best_by_category(my_priority)
-        
+    
         # invoking method swap_item() to swap item and it will return True or False
         return self.swap_items(other, my_best_item, other_best_item)
+        
+
+    # Optional Enhancements
+    def get_newest_by_category(self, category):
+        """Returning newest item if matching requirements, otherwise, return False"""
+        # if inventory and category are not empty then find the youngest age that is matching category for newest item
+        if category and len(self.inventory) > 0:
+            for item in self.inventory:
+                if item.category == category and isinstance(item.age, int):
+                    min_age = min(item.age for item in self.inventory if item.category == category)
+                    if item.age == min_age: 
+                        return item
+        return None
 
 
+    def swap_newest_by_category(self, other, my_priority, their_priority):
+        """Swapping by newest item of category and returnning invoked function swap_items() at the end."""
+        
+        # invoking method get_newest_by_category() to find my newest item and other newest item base on priority.
+        my_newest_item = self.get_newest_by_category(their_priority)
+        other_newest_item = other.get_newest_by_category(my_priority)
+
+        # invoking method swap_item() to swap item then return it
+        return self.swap_items(other, my_newest_item, other_newest_item)
+        
 
