20ffe4737b8977d46fbcfe33a84eec81a2d60a86
Shannon Bellemore <shannonbellemore@gmail.com>
2022-04-07 18:13:23 -0500

    wave 1, 2, 3 passing

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,16 @@
 class Item:
-    pass
+    def __init__(self, category=None):
+        if category is None:
+            self.category = ""
+        else:
+            self.category = category
+    
+    def __str__(self):
+        return "Hello World!"
+
+# ********************************
+# DELETE LATER
+# ********************************
+
+# ********************************
+# ********************************
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,42 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
+
+    def swap_items(self, friend, given_item, received_item):
+        if given_item in self.inventory and received_item in friend.inventory:
+            friend.add(given_item)
+            self.remove(given_item)
+            self.add(received_item)
+            friend.remove(received_item)
+            return True
+        return False 
+
+# ********************************
+# DELETE LATER
+# ********************************
+
+# ********************************
+# ********************************
