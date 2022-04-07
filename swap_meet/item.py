a9639ff0c1257621e11419f8a940214d4b535946
Natascha <njenkns@gmail.com>
2022-04-06 21:54:31 -0700

    added the age attribute for optional enhancement in Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,8 @@
 class Item:
-    def __init__(self, category="", condition=0): # str are immutable, so okay to use as default parameter
+    def __init__(self, category="", condition=0, age=0): # str are immutable, so okay to use as default parameter
         self.category = category
-        self.condition = condition 
+        self.condition = condition
+        self.age = age 
     
     def __str__(self): 
         return "Hello World!" 
