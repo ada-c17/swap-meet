291dc175d77e37bc57923d1ea0aa8dd958721b12
Hillary Smith <hillaryhsmith@gmail.com>
2022-04-06 17:28:37 -0700

    passed wave 5

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item 
+
+
+my_float = 3.5 
+
+my_trunc_float = my_float // 1
+
+my_int_float = int(my_float)
+print(my_trunc_float)
+print(my_int_float)
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,20 @@
 class Item:
-    def __init__(self, category = ''):
+    def __init__(self, category = '', condition = 0):
         self.category = category
+        self.condition = condition
     def __str__(self):
         return "Hello World!"
+        
+    def condition_description(self):
+        descriptions = { 
+            0: "I should be paying you to take it off my hands.",
+            1: "Just don't touch your face after handling.",
+            2: "No refunds.",
+            3: "Not too shabby.",
+            4: "Looking good!",
+            5: "Absolutely immaculate. Exquisitely rare find!"
+        }
+        
+        self.condition = int(self.condition)
+
+        return descriptions[self.condition]
