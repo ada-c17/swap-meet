bda3a25dbc0a3232d198aa1a0bc1924513c97ac4
Liliana <liliana.m.parra@gmail.com>
2022-04-07 17:27:46 -0400

    Implemented all classes and passed all tests

---
+++ main.py
@@ -0,0 +1,8 @@
+from swap_meet.vendor import Vendor
+from swap_meet.item import Item
+
+dry_item = Item()
+trenisha = Vendor([dry_item])
+
+print(vars(trenisha))
+
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,19 @@
 class Item:
-    pass
+    def __init__(self, category = "", condition = 0):
+        self.category = category
+        self.condition = condition
+    
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition > 5:
+            return "That's probably an item."
+
+        condition_descriptions = {0: "Yep, that's an item.",
+        1: "Previously owned", 2: "Previously despised",
+        3: "Previously tolerated", 4: "Previously enjoyed",
+        5: "Previously loved"}
+
+        return condition_descriptions[self.condition]
+
