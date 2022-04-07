234bd7ecaed5fa16757c09504f409ddc074708b6
Sabrina Huwang <sabrinahuwang1@gmail.com>
2022-04-06 23:28:50 -0700

    Wave 5 PASSED!

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,29 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__ (self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition < 1: 
+            return "BAD. Buy at your own risk"
+
+        elif self.condition < 2: 
+            return "Not the worst. Not the best"
+        
+        elif self.condition < 3: 
+            return "Decent!"
+        
+        elif self.condition < 4: 
+            return "Pretty good"
+        
+        elif self.condition <= 5: 
+            return "Good"
+        
+        else:
+            return "Try again!"
+    
+    
 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,10 +23,12 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
+
         self.remove(my_item)
         other_vendor.add(my_item)
         other_vendor.remove(their_item)
         self.add(their_item)
+        
         return True
     
     # Wave 4
