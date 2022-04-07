cffb1e1ac1e9479f81df18ded0ece53f02a6e796
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-06 17:55:19 -0700

    Updated so sub classes inherit applicable Item attributes and methods.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,7 +8,7 @@ class Item:
     def __str__(self):
         return "Hello World!"
 
-    def condition_descriptions (self):
+    def condition_description(self):
         if self.condition == 0:
             return f"terrible"
         if self.condition == 1:
@@ -16,8 +16,8 @@ class Item:
         if self.condition == 2:
             return f"kinda ok"
         if self.condition == 3:
-            return f"basic"
-        if self.condition == 4:
             return f"you'll do"
+        if self.condition == 4:
+            return f"basic"
         if self.condition == 5:
             return f"vibe"
