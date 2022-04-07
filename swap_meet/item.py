d48e9580064ac1a9fd04acfbf04471583c4cb2b0
K <karinrfrank@gmail.com>
2022-04-06 17:50:34 -0700

    Wave 2 test and Items class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,21 @@
 class Item:
-    pass
+    def __init__(self,category = "", condition = 0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Distressed"
+        if self.condition == 1:
+            return "Used fair"
+        if self.condition == 2:
+            return "Used good"
+        if self.condition == 3:
+            return "Used very good"
+        if self.condition == 4:
+            return "Used like new"
+        if self.condition ==5:
+            return "New"
