83d14985df10495766133538126dcea06c214187
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-06 17:54:46 -0700

    Updated to inherit applicable Item attributes and methods.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,22 +2,7 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     def __init__(self, category = "Electronics", condition = 0):
-        self.category = category
-        self.condition = condition
+        super(). __init__(category, condition)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
-
-    def condition_description(self):
-        if self.condition == 0:
-            return f"terrible"
-        if self.condition == 1:
-            return f"not great"
-        if self.condition == 2:
-            return f"kinda ok"
-        if self.condition == 3:
-            return f"basic"
-        if self.condition == 4:
-            return f"you'll do"
-        if self.condition == 5:
-            return f"vibe"
