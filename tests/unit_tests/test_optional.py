51a4fdca3975e040869ccd519deceb13cf871422
Morgan Buntin <mmbuntin@gmail.com>
2022-04-07 16:09:01 -0500

    Didn't need Item import

--- tests/unit_tests/test_optional.py
+++ tests/unit_tests/test_optional.py
@@ -3,7 +3,6 @@ from swap_meet.vendor import Vendor
 from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
-from swap_meet.item import Item
 
 def test_swap_by_newest():
     item_a = Clothing(age=2)
