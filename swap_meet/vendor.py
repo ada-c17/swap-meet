2eb4c82e367093c3bb217aa1c8e8de499d2104c4
Danielle <dmbecker24@gmail.com>
2022-04-06 18:10:23 -0700

    Replace repeated logic in  swap_first_item method  in Vendor class with swap_item method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,24 +28,20 @@ class Vendor:
         if my_item not in self.inventory or their_item not in vendor_b.inventory:
             return False
 
+        self.inventory.append(their_item)
+        vendor_b.inventory.append(my_item)
         self.inventory.remove(my_item)
         vendor_b.inventory.remove(their_item)
-        if their_item.category not in self.inventory:
-            self.inventory.append(their_item)
-            vendor_b.inventory.append(my_item)
         return True
 
     def swap_first_item(self, vendor_b):
         
         if not self.inventory or not vendor_b.inventory:
             return False
-        
-        self.inventory.append(vendor_b.inventory[0])
-        vendor_b.inventory.append(self.inventory[0])
-        self.inventory.remove(self.inventory[0])
-        vendor_b.inventory.remove(vendor_b.inventory[0])
             
-        return True
+        swapped_items = self.swap_items(vendor_b, self.inventory[0], vendor_b.inventory[0])
+            
+        return swapped_items
 
     def get_best_by_category(self, category):
         items_match_category = self.get_by_category(category)
