a49e70b9b8dbbae4c8d66964a36843ebfda0886d
Natascha <njenkns@gmail.com>
2022-04-06 18:41:18 -0700

    passed tests in wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,11 +17,11 @@ class Vendor:
         return False 
 
     def get_by_category(self, category):
-        items_with_categories = [] 
+        items_same_categories = [] 
         for item in self.inventory: # O(n)
             if item.category == category: # O(1)
-                items_with_categories.append(item) # O(1)      
-        return items_with_categories 
+                items_same_categories.append(item) # O(1)      
+        return items_same_categories 
 
     def swap_items(self, vendor, my_item, their_item):
         if my_item in self.inventory and their_item in vendor.inventory:
@@ -33,7 +33,7 @@ class Vendor:
         return False 
     
     def swap_first_item(self, vendor):
-        if len(self.inventory) and len(vendor.inventory) != 0: # why doesn't len(self.inventory) or len(vendor.inventory) == 0: work?
+        if len(self.inventory) and len(vendor.inventory) != 0: 
             my_first_item = self.inventory[0]
             their_first_item = vendor.inventory[0]
             self.remove(my_first_item)
@@ -42,6 +42,23 @@ class Vendor:
             self.add(their_first_item)
             return True
         return False 
+    
+    def get_best_by_category(self, category):
+        items_same_categories = self.get_by_category(category)
+        item_best_condition = None
+        max_condition = 0.0
+        for item in items_same_categories:
+            while item.condition > max_condition:
+                max_condition = item.condition
+                item_best_condition = item 
+        return item_best_condition
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        vendor_best_item_by_category = self.get_best_by_category(their_priority)
+        other_best_item_by_category = other.get_best_by_category(my_priority)
+        best_swapped_items = self.swap_items(other, vendor_best_item_by_category, other_best_item_by_category)
+        return best_swapped_items 
+
 
 
 
