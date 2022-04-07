12daf49adc6ef003d905f17e9911854fcbaadf28
Amber <ambertanaka@yahoo.com>
2022-04-06 17:48:44 -0700

    passing first integration test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,11 @@
 from operator import attrgetter
 
 class Vendor:
-    def __init__(self, inventory = []):
-        self.inventory = inventory 
+    def __init__(self, inventory = None):
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory 
 
     def add(self, item):
         self.inventory.append(item)
