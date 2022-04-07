864c71c8eb8c4f33f16d32db36c51f89ecbc8737
Sreymompech <sanchhen2013@gmail.com>
2022-04-07 00:36:57 -0700

    In Electronics class, refactoring __init__() by adding inheritance of age attribute for optional enhancements

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,15 +3,18 @@ class Electronics(Item):
     """Child class that inherits attributes and methods from Item class as parent class:"""
     
 
-    def __init__(self, condition = None):
-        """Setting category as default with "Electronics" which inherits attributes from Item class."""
-        super().__init__(category= "Electronics", condition = condition)
+    def __init__(self, condition = None, age = None):
+        """Setting category attribute as default with "Electronics" and inherits attributes from Item class."""
+        super().__init__(category= "Electronics", condition = condition, age = age)
 
 
     def __str__(self):
         """Overriding method from Item class by returning differenct value in string format if condition is empty."""
         if not self.condition:
             return "A gadget full of buttons and secrets."
+
+    
+
 
 
     
