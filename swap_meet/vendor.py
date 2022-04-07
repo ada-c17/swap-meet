426b426cf06d3a85fa22891cf1015f0cee4152a9
NinaPatrina <ninochca@gmail.com>
2022-04-07 15:44:06 -0700

    closures for wave 7

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -60,46 +60,46 @@ class Vendor:
 
     #* I know, that I used decorators in an unusual way, because when I started to implement them, I did not fully understand them and I thought we use decorators to change the behaviour of the function. Now I learned that decorators should not change but extend the behaviour of the function. However because they work and I learned through this exersize a lot - I decided to leave the code as it is. I learned my lesson and promise not to use decorators for changing the behavior of the function again. Below in comments you can find a correct way to implement my idea that Matt McKnett recommendended.
     #  
-    f_max_min ={ 'age': min,'condition':max}
+    # f_max_min ={ 'age': min,'condition':max}
 
-    def attr_decorator(attr):
-        def a_decorator_passing_arguments(method_to_decorate):
-                def a_wrapper_accepting_arg(self, category):
-                    matching_category=self.get_by_category(category)
-                    return  self.f_max_min[attr](matching_category, key =attrgetter(attr), default=None)
-                return a_wrapper_accepting_arg
-        return a_decorator_passing_arguments          
+    # def attr_decorator(attr):
+    #     def a_decorator_passing_arguments(method_to_decorate):
+    #             def a_wrapper_accepting_arg(self, category):
+    #                 matching_category=self.get_by_category(category)
+    #                 return  self.f_max_min[attr](matching_category, key =attrgetter(attr), default=None)
+    #             return a_wrapper_accepting_arg
+    #     return a_decorator_passing_arguments          
 
-    @attr_decorator(attr='condition')
-    def get_best_by_category(self, category):
-        pass
+    # @attr_decorator(attr='condition')
+    # def get_best_by_category(self, category):
+    #     pass
     
-    @attr_decorator(attr='age')
-    def get_best_by_age(self, category):
-        pass
+    # @attr_decorator(attr='age')
+    # def get_best_by_age(self, category):
+    #     pass
     
-    funcs={ 'age': get_best_by_age,
-            'condition':get_best_by_category}
+    # funcs={ 'age': get_best_by_age,
+    #         'condition':get_best_by_category}
 
-    def attr_decorator(attr):
-        def a_decorator_passing_arguments(method_to_decorate):           
-            def a_wrapper_accepting_arg(self,other,my_priority, their_priority):
-                    my_best_item=self.funcs[attr](self, their_priority)
-                    their_best_item=other.funcs[attr](other, my_priority)
-                    if my_best_item is None or their_best_item is None:
-                        return False
-                    self.swap_items(other, my_best_item, their_best_item)
-                    return True         
-            return a_wrapper_accepting_arg
-        return a_decorator_passing_arguments        
+    # def attr_decorator(attr):
+    #     def a_decorator_passing_arguments(method_to_decorate):           
+    #         def a_wrapper_accepting_arg(self,other,my_priority, their_priority):
+    #                 my_best_item=self.funcs[attr](self, their_priority)
+    #                 their_best_item=other.funcs[attr](other, my_priority)
+    #                 if my_best_item is None or their_best_item is None:
+    #                     return False
+    #                 self.swap_items(other, my_best_item, their_best_item)
+    #                 return True         
+    #         return a_wrapper_accepting_arg
+    #     return a_decorator_passing_arguments        
 
-    @attr_decorator(attr='condition')
-    def swap_best_by_category(self,other,my_priority, their_priority):
-        return True
+    # @attr_decorator(attr='condition')
+    # def swap_best_by_category(self,other,my_priority, their_priority):
+    #     return True
 
-    @attr_decorator(attr='age')
-    def swap_by_newest(self,other,my_priority, their_priority):
-        return True    
+    # @attr_decorator(attr='age')
+    # def swap_by_newest(self,other,my_priority, their_priority):
+    #     return True    
 
     #* the code above should be refactored to this:
 
@@ -115,3 +115,32 @@ class Vendor:
     
     # def get_best_by_age(self, category):
     #     return self.get_best_by_attr(category, 'age')
+
+
+    # me again. Day3. Learning closures. another way to implement the code. maybe not the best way, but its cool that there are so many different ways to achive the same goal. 
+
+    f_max_min ={ 'age': min,'condition':max}
+    def using_closures(attr):                               
+        def get_best_by(self, category):
+            matching_category=self.get_by_category(category)
+            return  self.f_max_min[attr](matching_category, key =attrgetter(attr), default=None)
+        return get_best_by
+    get_best_by_category=using_closures('condition')    
+    get_best_by_age=using_closures('age')
+    
+    
+    funcs={ 'age': get_best_by_age,
+            'condition':get_best_by_category}
+
+    def using_closures2(attr):
+        def swap_best_by(self,other,my_priority, their_priority):
+            my_best_item=self.funcs[attr](self, their_priority)
+            their_best_item=other.funcs[attr](other, my_priority)
+            if my_best_item is None or their_best_item is None:
+                    return False
+            self.swap_items(other, my_best_item, their_best_item)
+            return True         
+        return swap_best_by
+
+    swap_best_by_category=using_closures2('condition')  
+    swap_by_newest=using_closures2('age')
