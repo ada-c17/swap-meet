9fd73b64430a27165428f1228081486d7b11bd07
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-04-06 22:50:40 -0700

    Removed unnecessary comments

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -7,21 +7,3 @@ class Clothing(Item):
 
     def __str__(self):
         return "The finest clothing you could wear."
-
-    # def condition_description(self):
-    #     # 0 - 1 --> "Don't touch with bare hands"
-    #     # 1 - 2 --> "Is it worth it? Probably not."
-    #     # 2 - 3 --> "Meh, it's neutral."
-    #     # 3 - 4 --> "It's a good find."
-    #     # 4 - 5 --> "Top tier item! Must have."
-
-    #     if (self.condition >= 0) and (self.condition <= 1):
-    #         return "Don't touch with bare hands."
-    #     elif (self.condition > 1) and (self.condition <= 2):
-    #         return "Is it worth it? Probably not."
-    #     elif (self.condition > 2) and (self.condition <= 3):
-    #         return "Meh, it's neutral."
-    #     elif (self.condition > 3) and (self.condition <= 4):
-    #         return "It's a good find."
-    #     elif (self.condition > 4) and (self.condition <= 5):
-    #         return "Top tier item! Must have."
