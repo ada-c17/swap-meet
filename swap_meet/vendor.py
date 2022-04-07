79fe9e9cfc88d2fd8d4c06a1ce58ea8fe341133b
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-04-07 10:57:25 -0700

    Wave 6 completed, all tests passed, project completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -37,3 +37,21 @@ class Vendor:
             return False
         self.swap_items(friend, self.inventory[0], friend.inventory[0])
         return True
+
+    def get_best_by_category(self, category):
+        categoty_items = self.get_by_category(category)
+        if categoty_items == []:
+            return None
+        best_item = categoty_items[0]
+        for item in categoty_items:
+            if item.condition > best_item.condition:
+                best_item = item
+        return best_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+        if my_best_item is None or their_best_item is None:
+            return False
+        self.swap_items(other, my_best_item, their_best_item)
+        return True
