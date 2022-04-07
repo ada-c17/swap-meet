6c162ab6c7ccaebc46ce856ed5c6c25cbb9861fa
Amel <amelntamark@gmail.com>
2022-04-07 14:34:56 -0700

    change default inventory for new Vendor from empty list to None

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,11 @@
 from .item import Item
 
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        if inventory is None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
 
     def add(self, item):
         self.inventory.append(item)
