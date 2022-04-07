a842ec01e30ed1bf3d25425f58978fd85ff57e67
Sabrina Huwang <sabrinahuwang1@gmail.com>
2022-04-06 18:51:29 -0700

    Wave 3 DONE. I am so bad at reading comphrehension omg

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+# Wave 1 
 class Vendor:
     def __init__(self, inventory=[]):
         self.inventory = inventory
@@ -13,11 +14,22 @@ class Vendor:
             self.inventory.remove(item)
             return item
 
+    # Wave 2
     def get_by_category(self, category):
         items = [item for item in self.inventory if item.category == category]
         return items
 
-    
-    
+    # Wave 3
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        other_vendor.remove(their_item)
+        self.add(their_item)
+        return True
+
+        
+        
 
 
