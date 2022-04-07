ecd65c89917d29928a605ca0f3de52391ae81565
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-06 19:06:06 -0700

    updates made as part of refactoring

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,86 +24,69 @@ class Vendor:
                 items_in_category.append(item)
         return items_in_category
 
-    def swap_items(self, other_vendor, my_item, their_item):
-        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+    def swap_items(self, other, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other.inventory:
             return False
         else:
-            self.remove(my_item)
-            other_vendor.add(my_item)
-            
-            other_vendor.remove(their_item)
-            self.add(their_item)
-        return True
-
-    def swap_first_item(self, other_vendor):
-        if not self.inventory or not other_vendor.inventory:
-            return False
-        else:
-            my_first_item = self.remove(self.inventory[0])
-            other_vendor.add(my_first_item)
-            
-            thier_first_item = other_vendor.remove(other_vendor.inventory[0])
-            self.add(thier_first_item)
-        return True
-
-    def get_best_by_category(self, category): #refactor to see if you can use get_category()
-
-        condition_of_items_in_category = []
-
-        for item in self.inventory:
-            if category == item.category:
-                condition_of_items_in_category.append(item.condition)
-        
-        if not condition_of_items_in_category:
-            return None
-        else:
-            best_condition = max(condition_of_items_in_category)
-        
-        for item in self.inventory:
-            if category == item.category and item.condition == best_condition:
-                return item
-
-
-
-        # if not get_category_items_list:
-        #     return None
-        # else:
-        #     return max(get_category_items_list.item.condition)
-
-    def swap_best_by_category(self, other, my_priority, their_priority):
-        if not self.inventory or not other.inventory:
-            return False
-        
-        this_vendor_condition_best_item = []
-        other_vendor_condition_best_item = []
-
-        for item in self.inventory:
-            this_vendor_condition_best_item.append(item.condition)
-
-        for item in other.inventory:
-            other_vendor_condition_best_item.append(item.condition)
-        
-        this_vendor_best_condition = max(this_vendor_condition_best_item)
-        other_vendor_best_condition = max(other_vendor_condition_best_item)
-
-        my_trade_is_valid = False
-        for item in self.inventory:
-            if their_priority == item.category and item.condition == this_vendor_best_condition:
-                my_item = item
-                my_trade_is_valid = True
-
-        their_trade_is_valid = False
-        for item in other.inventory:
-            if my_priority == item.category and item.condition == other_vendor_best_condition:
-                their_item = item
-                their_trade_is_valid = True
-
-        if my_trade_is_valid and their_trade_is_valid:
             self.remove(my_item)
             other.add(my_item)
             
             other.remove(their_item)
             self.add(their_item)
-            return True
-        else:
+        return True
+
+    def swap_first_item(self, other):
+        if not self.inventory or not other.inventory:
             return False
+        else:
+            return self.swap_items(other, self.inventory[0], other.inventory[0])
+
+    def get_best_by_category(self, category):
+
+        condition_of_items_in_category = []
+
+        for item in self.get_by_category(category):
+                condition_of_items_in_category.append(item.condition)
+        
+        if not condition_of_items_in_category:
+            return None
+
+        best_condition = max(condition_of_items_in_category) #refactor with list comprehension?
+
+        if not best_condition:
+            return None
+        
+        for item in self.get_by_category(category):
+            if item.condition == best_condition:
+                return item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        if not self.inventory or not other.inventory:
+            return False
+        
+        condition_my_best_item = []
+        for item in self.inventory:
+            condition_my_best_item.append(item.condition)
+
+        my_best_item = max(condition_my_best_item)
+
+        condition_their_best_item = []
+        for item in other.inventory:
+            condition_their_best_item.append(item.condition)
+        
+        other_vendor_best_item = max(condition_their_best_item)
+
+        my_trade_is_valid = False
+        for item in self.get_by_category(their_priority):
+            if item.condition == my_best_item:
+                my_item = item
+                my_trade_is_valid = True
+
+        their_trade_is_valid = False
+        for item in other.get_by_category(my_priority):
+            if item.condition == other_vendor_best_item:
+                their_item = item
+                their_trade_is_valid = True
+
+        if my_trade_is_valid and their_trade_is_valid:
+            return self.swap_items(other, my_item, their_item)
