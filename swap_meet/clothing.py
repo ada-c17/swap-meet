d83059476b8d06eac683910cb7489f2944e14877
Jamie H <jamiewhorvath@gmail.com>
2022-04-08 00:22:36 -0400

    Major submission of vendor, item, electronics, decor, clothing classes and test assertions

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,19 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+    '''
+    Class representing a clothing item to be traded
+    Child of Item class
+
+    Methods:
+        condition_description() from Item class
+
+    Variables:
+        category: category of clothing default is Clothing
+        condition: condition of an item from 0-5
+    '''
+    def __init__(self, category="Clothing", condition=0):
+        super().__init__(category, condition)
+
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,107 @@
 class Vendor:
-    pass
+    '''
+    Class representing a single vendor with items to trade
+
+    Functions:
+        add(object, item)
+        remove(object, item)
+        get_by_category(object, category)
+        swap_items(object, another object, my_item, their_item)
+        swap_first_item(object, another object)
+        get_best_by_category(self, category):
+        swap_best_by_category(self, other, my_priority, their_priority):
+
+    Variables:
+        inventory: list of items for a vendor to see default is [] 
+    '''
+    def __init__(self, inventory=None):
+        if not inventory == None:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+        # self.inventory = inventory if inventory is not None else []
+
+
+    def add(self, item):
+        """Method to add passed item to instances inventory"""
+        self.inventory.append(item)
+
+        return item
+
+
+    def remove(self, item):
+        """Method to remove passed item to instances inventory"""
+        if not item in self.inventory:
+            return False
+
+        self.inventory.remove(item)
+
+        return item
+
+
+    def get_by_category(self, category):
+        """Method to return a list of items of a passed category"""
+        category_list = []
+
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+
+        return category_list
+
+
+    def swap_items(self, other, my_item, their_item):
+        """Method to passed items of two Vendor Classes"""
+        if (not my_item in self.inventory 
+            or not their_item in other.inventory):
+            return False
+        
+        self.add(their_item)
+        other.add(my_item)
+
+        self.remove(my_item)
+        other.remove(their_item)
+
+        return True
+
+
+    def swap_first_item(self, other):
+        """Method to swap first items of two Vendor Classes"""
+        if self.inventory == [] or other.inventory == []:
+            return False
+
+        self.swap_items(other, self.inventory[0], other.inventory[0])
+
+        return True
+
+
+    def get_best_by_category(self, category):
+        """Method to return the highest condition item of a category"""
+        category_list = self.get_by_category(category)
+        if not category_list:
+            return None
+        
+        best = category_list[0]
+        print(best)
+
+        for item in category_list:
+            if item.condition > best.condition:
+                best = item
+
+        return best
+        
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        """
+        Method to swap two vendors best condition item of 
+        the others chosen category
+        """
+        my_item = self.get_best_by_category(their_priority)
+        their_item = other.get_best_by_category(my_priority)
+
+        if not my_item or not their_item:
+            return False
+
+        self.swap_items(other, my_item, their_item)
+
+        return True
