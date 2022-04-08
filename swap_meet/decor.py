eadc512e0fd99288bbbc6d4402319ceb5f353779
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-07 22:21:07 -0700

    Added docstrings

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,12 @@
 from swap_meet.item import Item
+'''
+Decor is a child class of Item. It is a type of Item (DecorItem).
+Its attribute category has a namesake string value.
+'''
 
 class Decor(Item):
-    def __init__(self, category = "Decor", condition = 0):
-        super().__init__(category, condition)
+    def __init__(self, condition = 0):
+        super().__init__(category = "Decor", condition = condition)
 
     def __str__(self):
         return "Something to decorate your space."
