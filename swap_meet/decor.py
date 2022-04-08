70431741b7e39cae38736226786b7e2cc34789bc
Sreymompech <sanchhen2013@gmail.com>
2022-04-07 19:37:27 -0700

    In Decor class, re-setting 0 as default value of condition and age attributes in __init__()

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -3,14 +3,13 @@ class Decor(Item):
     """Child class that inherits attributes and methods from Item class as parent class:"""
 
 
-    def __init__(self, condition = None, age = None):
+    def __init__(self, condition = 0, age = 0):
         """Setting category attribute as default with "Decor" and inherits attributes from Item class."""
         super().__init__(category= "Decor", condition = condition, age = age)
         
 
     def __str__(self):
         """Printing the returnning message string when printing instance of Decor class."""
-        
         return "Something to decorate your space."
 
 
