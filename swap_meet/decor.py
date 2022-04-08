003357786e512e23b41277e2dd23eabbc4566756
shelby <faulconershelby@gmail.com>
2022-04-08 16:58:59 -0700

    edited decor.py for consistent whitespace

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -5,5 +5,6 @@ class Decor(Item):
     def __init__(self, condition = 0):
         super().__init__(category= "Decor", condition = condition)   
 
+
     def __str__(self):
         return "Something to decorate your space."
