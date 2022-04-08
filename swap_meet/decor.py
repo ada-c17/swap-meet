b280c557158f2f57724c0dcf67a6f3bf9a58cf1e
Doina Colun <doinacolun@macpro.local>
2022-04-07 22:23:59 -0700

    Final version of swap-meet project

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,11 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self, condition=0):
+        super().__init__(condition=condition)
+        self.category = "Decor"
+
+    def __str__(self):
+        return "Something to decorate your space."
+
+            
