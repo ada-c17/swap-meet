8ceb1af07010d0352810acaef155bcde50ad7d49
Marlyn <marlynlopezz@gmail.com>
2022-04-07 17:08:00 -0700

    test swap best no match false assert statements

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -179,7 +179,7 @@ def test_swap_best_by_category_no_other_inventory_is_false():
     assert item_b in tai.inventory
     assert item_c in tai.inventory
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -203,14 +203,19 @@ def test_swap_best_by_category_no_match_is_false():
         their_priority="Clothing"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
     # Assertions should check:
     # - That result is falsy
+    assert result == False
     # - That tai and jesse's inventories are the correct length
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
     # - That all the correct items are in tai and jesse's inventories
+    assert tai.inventory== [item_a, item_b, item_c]
+    assert jesse.inventory== [item_d, item_e, item_f]
 
 @pytest.mark.skip
 def test_swap_best_by_category_no_other_match_is_false():
