88dd8a9ca827a2df8b1ca3ed648dac43c7b751bd
Shannon Bellemore <shannonbellemore@gmail.com>
2022-04-08 09:59:12 -0500

    passing all waves

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,16 +1,20 @@
 class Item:
-    def __init__(self, category=None):
-        if category is None:
-            self.category = ""
-        else:
-            self.category = category
-    
+    def __init__(self, category="", condition=0.0):
+        self.category = category
+        self.condition = condition
+
     def __str__(self):
         return "Hello World!"
 
-# ********************************
-# DELETE LATER
-# ********************************
-
-# ********************************
-# ********************************
+    def condition_description(self):
+        if self.condition <= 1:
+            return "Belongs in the trash."
+        elif self.condition <= 2:
+            return "Oof...bad idea."
+        elif self.condition <= 3:
+            return "Is it really worth it?"
+        elif self.condition <= 4:
+            return "Heyyyy, this is pretty good~"
+        elif self.condition <= 5:
+            return "Brilliant, incredible, amazing, show stopping, spectacular item!"   
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,18 +25,38 @@ class Vendor:
                 items.append(item)
         return items
 
-    def swap_items(self, friend, given_item, received_item):
-        if given_item in self.inventory and received_item in friend.inventory:
-            friend.add(given_item)
-            self.remove(given_item)
-            self.add(received_item)
-            friend.remove(received_item)
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item)
+            self.remove(my_item)
+            self.add(their_item)
+            friend.remove(their_item)
             return True
-        return False 
+        else:
+            return False
 
-# ********************************
-# DELETE LATER
-# ********************************
+    def swap_first_item(self, friend):
+        if not self.inventory or not friend.inventory:
+            return False
+        else:
+            self.swap_items(friend, self.inventory[0], friend.inventory[0])
+            return True
 
-# ********************************
-# ********************************
+    def get_best_by_category(self, category=""):
+        current_best_condition = 0
+        best_item = None
+        if self.get_by_category is None:
+            return None
+        for item in self.get_by_category(category):
+            if item.condition > current_best_condition:
+                current_best_condition = item.condition
+                best_item = item
+        return best_item
+        
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        other_best = other.get_best_by_category(my_priority)
+        vendor_best = self.get_best_by_category(their_priority)
+        if not other_best or not vendor_best:
+            return False
+        else:
+            return self.swap_items(other, vendor_best, other_best)
