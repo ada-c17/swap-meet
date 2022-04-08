0a1c3196123f0bf9740d494cdfdbcb627702bed4
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-04-07 21:37:59 -0400

    adds additional tests to complete coverage

--- tests/unit_tests/test_additional_cases.py
+++ tests/unit_tests/test_additional_cases.py
@@ -27,3 +27,30 @@ def test_removing_from_inventory_takes_multiple_items():
 
     assert len(vendor.inventory) == 1
     assert vendor.inventory[0] == b
+
+def test_removing_from_inventory_raises_error_w_dict():
+    a, b, c = Clothing(), Decor(), Electronics()
+    vendor = Vendor([a, b, c])
+
+    dict_to_remove = {'item_a': a,'item_c': c}
+
+    with pytest.raises(ValueError):
+        vendor.remove(dict_to_remove)
+
+def test_adding_to_inventory_does_not_add_duplicates():
+    a, b, c = Clothing(), Decor(), Electronics()
+    vendor = Vendor([a, b])
+
+    vendor.add([b, c])
+
+    assert len(vendor.inventory) == 3
+
+def test_when_removing_multiple_items_no_action_taken_if_any_item_not_found():
+    a, b, c = Clothing(), Decor(), Electronics()
+    vendor = Vendor([a, b])
+
+    result = vendor.remove((a,c))
+
+    assert len(vendor.inventory) == 2
+    assert a in vendor.inventory
+    assert result == False
