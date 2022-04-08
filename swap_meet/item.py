9385d272836e373300b9c11148afb69c99820c91
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-07 22:23:57 -0700

    Delete comments, add docstrings, minor changes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,13 @@
 class Item:
+    '''
+    Item is the parent class to the Clothing, Decor, and Electronics classes, which inherit
+    Item's attributes and methods. Instances of the Vendor class (vendors) can have
+    Item class elements as elements of their inventories.
+
+    The Item class' method condition_description returns a label (string) based on what
+    value is passed onto its attribute condition.
+    '''
+
     def __init__(self, category = "", condition = 0):
         self.category = category
         self.condition = condition
@@ -9,8 +18,8 @@ class Item:
     def condition_description(self):
         ratings = {
             0: "Quite used, but once super cute - could refactor it like bad code!",
-            2: "It's been through a lot",
-            1: "Verrry used, but quality fabric and fit",
+            1: "It's been through a lot",
+            2: "Verrry used, but quality fabric and fit",
             3: "Passable",
             4: "Mint condition, sparkly and all",
             5: "Nevern worn, labels and all, not like new -- it IS new!"
