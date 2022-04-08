52d7fd80e9608d31ef191374ecceb9754149cbc7
Sreymompech <sanchhen2013@gmail.com>
2022-04-07 19:35:22 -0700

    In Clothing class, re-setting 0 as default value of condition and age attributes in __init__()

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,14 +3,13 @@ class Clothing(Item):
     """Child class that inherits attributes and methods from Item class as parent class:"""
 
 
-    def __init__(self, condition = None, age = None):
+    def __init__(self, condition = 0, age = 0):
         """Setting category attribute as default with "Clothing" and inherits attributes from Item class."""
         super().__init__(category= "Clothing", condition = condition, age = age)
         
 
     def __str__(self):
         """Printing the returnning message string when printing instance of Clothing class."""
-        
         return "The finest clothing you could wear."
 
     
