60b8c46a84a33b3de2c8ec75887377b1610bd020
Roshni Patel <rpatel01@wesleyan.edu>
2022-04-08 08:20:17 -0700

    Update condition description method and add test to reach 100% code coverage

--- tests/unit_tests/test_wave_05.py
+++ tests/unit_tests/test_wave_05.py
@@ -53,7 +53,7 @@ def test_items_have_condition_descriptions_that_are_the_same_regardless_of_type(
 
     assert one_condition_description != five_condition_description
 
-def test_items_raises_exception_when_condition_is_not_between_0_and_5_():
+def test_items_returns_error_when_condition_is_not_between_0_and_5_():
     # Arrange
     item = Electronics(condition=6)
 
@@ -62,3 +62,20 @@ def test_items_raises_exception_when_condition_is_not_between_0_and_5_():
 
     # Assert
     assert result == "Condition must be a float between 0 and 5."
+
+def test_items_returns_correct_condition_description_for_floats():
+    # Arrange
+    item_a = Decor(condition=0.4, age=3.0)
+    item_b = Electronics(condition=1.2, age=5.0)
+    item_c = Decor(condition=2.5, age=2.0)
+    item_d = Decor(condition=3.6, age=3.0)
+    item_e = Electronics(condition=4.2, age=5.0)
+    item_f = Decor(condition=5.0, age=2.0)
+
+    # Act/Assert 
+    assert item_a.condition_description() == "Poor"
+    assert item_b.condition_description() == "Acceptable"
+    assert item_c.condition_description() == "Good"
+    assert item_d.condition_description() == "Very Good"
+    assert item_e.condition_description() == "Like New"
+    assert item_f.condition_description() == "Brand New"
