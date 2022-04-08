953c1380c55a82a83c4b4fcf5d3ac6db4fa55634
Danielle <dwhyte1226@gmail.com>
2022-04-07 21:38:36 -0500

    added logic to validate value of condit ion in the initializer for Item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,7 +3,9 @@ class Item:
 
     def __init__(self, category = "", condition = 0.0):
         self.category = category
-        self.condition = condition #add check that condition is within 0 and 5
+        if condition < 0 or condition > 5: #check that condition is between 0 and 5
+            raise ValueError
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
