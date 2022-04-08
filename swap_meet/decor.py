2aa7efa56c08bcdcaabf5de04ce4c06b0ab68a69
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-08 06:16:36 -0700

    docstrings added

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,10 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, category = "Decor", condition = 0):
-        super(). __init__(category, condition)
+    '''Decor class is instantiated and inherits from Item class'''
+
+    def __init__(self, category = "Decor", condition = 0, age = 0):
+        super(). __init__(category, condition, age)
 
     def __str__(self):
         return "Something to decorate your space."
