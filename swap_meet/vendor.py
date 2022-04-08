734a29420c6b8950978d05da7f21d94048ad1b0e
verasazonova <vera.zhokhova@gmail.com>
2022-04-07 23:14:53 -0700

    Refactoring

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,27 +1,25 @@
 class Vendor():
-    # create inventory parameters, where deafalt is empty list
     def __init__(self,inventory = None):
         if inventory is None:
             inventory = []
         self.inventory = inventory
     
 
-    # create instance method add which add things in inventory list
+    '''Instance method add, which add itens in the inventory list'''
     def add(self,item):
         self.inventory.append(item)
         return item
 
 
-    # create instance method remove which remove item from inventory list
+    '''Instance method remove which remove item from inventory list'''
     def remove(self,item):
-        for items in self.inventory:
-            if item == items:
-                self.inventory.remove(items)
-                return items
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
         return False
 
 
-    # create instance method which return a list of Items in the inventory with that category
+    '''Instance method which return a list of items in the inventory with that category'''
     def get_by_category(self,category):
         list_of_items = []
         for item in self.inventory:
@@ -30,51 +28,33 @@ class Vendor():
         return list_of_items
 
 
-    # create instance method implement swaping process
+    '''Instance method implement swaping process between vendor and other person'''
     def swap_items(self, friend, my_item, friends_item):
-        
-        if not my_item or not friends_item:
-            return False
-        
-        else:
-            if my_item in self.inventory and friends_item in friend.inventory:
-                # vendor swap his item
-                self.remove(my_item)
-                friend.add(my_item)
-                # friend swap his item
-                friend.remove(friends_item)
-                self.add(friends_item)
-                return True
-
-    # create instance method implement swaping process first item
-    def swap_first_item(self, friend):
-
-        if len(self.inventory) == 0 or len(friend.inventory) == 0:
-            return False
-        else:
-            # grab first element from self list 
-            first_item = self.inventory[0]
-            # remove from self inventory
-            self.remove(first_item)
-            # add to friends inventory 
-            friend.add(first_item)
-
-            # grab first element from friends list
-            first_item = friend.inventory[0]
-            # remove this elem from friensd list
-            friend.remove(first_item)
-            # add to self inventory
-            self.add(first_item)
+        if my_item in self.inventory and friends_item in friend.inventory:
+            # vendor swap his item
+            self.remove(my_item)
+            friend.add(my_item)
+            # friend swap his item
+            friend.remove(friends_item)
+            self.add(friends_item)
             return True
+        return False
 
+
+    '''Instance method implement swaping process first item'''
+    def swap_first_item(self, friend):
+        if len(self.inventory) and len(friend.inventory) != 0:
+            self.swap_items(friend,self.inventory[0],friend.inventory[0])
+            return True
+        return False
+
+
+    '''Instance method will get the item with the best condition in a certain category'''
     def get_best_by_category(self, consider_category):
-
-        # creating dictionary to store matching category with highet rate
         best_dict_category = {}
         best_dict_category[consider_category] = 0.0
         # creating counter to check no matches categories in vendors inventory
         count = 0
-
         # consider given represents string of category, add to dictionary 
         # matching category is key and value is rate. Setup first element
         # of dictionary like max and matching other rate, if higher add to dict 
@@ -88,29 +68,14 @@ class Vendor():
                 return None
         return best_category
 
+    '''Instance method will swap the item with the best condition in a certain category'''
     def swap_best_by_category(self, other, my_priority, their_priority):
-
-        if len(self.inventory) == 0 or len(other.inventory) == 0:
-            return False
-        else:
-            # get best result of category by prioriry for friend
+        if len(self.inventory) and len(other.inventory) != 0:
             best_item_for_friend = self.get_best_by_category(their_priority)
-            print(best_item_for_friend)
-            # get best result of category by prioriry for vendor
             best_item_for_vendor = other.get_best_by_category(my_priority)
-            print(best_item_for_vendor)
-
-            if best_item_for_friend == None or best_item_for_vendor == None:
+            if best_item_for_friend is None or best_item_for_vendor is None:
                 return False
             else:
-                #swapping items: 
-                #remove from self inventory
-                self.remove(best_item_for_friend)
-                # add to friends inventory 
-                other.add(best_item_for_friend)
-                # remove this elem from friensd list
-                other.remove(best_item_for_vendor)
-                # add to self inventory
-                self.add(best_item_for_vendor)
-
+                self.swap_items(other,best_item_for_friend,best_item_for_vendor)
                 return True
+        return False
