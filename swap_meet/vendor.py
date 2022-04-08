068fde7dc7462a718e047f62f02c5b7780848512
MandyG415 <missmandygao@gmail.com>
2022-04-07 21:02:31 -0700

    Added Docstirngs

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,14 +1,20 @@
 class Vendor():
   def __init__ (self, inventory = None):
-    if inventory is None: #do not want mutable object
-      inventory = [] #as a parameter, so set to None
-    self.inventory = inventory #empty list
+    """Constructor with parameter as None. The parameter
+    cannot be a mutable object, but is defined in the code below"""
+    if inventory is None:
+      inventory = []
+    self.inventory = inventory
   
   def add(self, item):
+    """Add instance method where item is appended to
+    the instance self.inventory, return item"""
     self.inventory.append(item)
     return item
 
   def remove(self, item):
+    """remove instance method where item removed if
+    it is checked in self.inventory instance"""
     if item in self.inventory:
       self.inventory.remove(item)
       return item
@@ -16,13 +22,19 @@ class Vendor():
       return False
 
   def get_by_category(self,string_category):
+    """This function recieves a string parameter, and checks
+    multiple items if they have the same category. The ouput is a list
+    of items with the same category"""
     inventory_same_category = []
     for item in self.inventory:
-      if item.category == string_category: #we want category here
+      if item.category == string_category: 
         inventory_same_category.append(item) #only append item associated with category
-    return inventory_same_category#list of inventory
+    return inventory_same_category
   
   def swap_items(self, vendor, my_item, their_item):
+    """This function receives three parameters that will
+    be used when swapping items from self/vendor and my_item
+    with their_item"""
     if my_item in self.inventory and their_item in vendor.inventory:
       self.inventory.remove(my_item)
       vendor.inventory.append(my_item)
@@ -33,6 +45,9 @@ class Vendor():
       return False
 
   def swap_first_item(self, vendor):
+    """This function checks to make sure the list has at least
+    one element, and switches the first element between self and vendor.
+    The helper function swap_items is used"""
     if len(vendor.inventory) and len(self.inventory) != 0:
       self_first_item = self.inventory[0]
       vendor_first_item = vendor.inventory[0]
@@ -42,6 +57,9 @@ class Vendor():
       return False
 
   def get_best_by_category(self, string_category):
+    """This function uses the helper function get_by_category 
+    to get the condition of each catgory, finds the best condition
+    and returns the item associated"""
     max_condition = 0
     stored_item = None 
     inventory_by_cat = self.get_by_category(string_category)
@@ -52,12 +70,18 @@ class Vendor():
     return stored_item
 
   def swap_best_by_category(self,other,my_priority,their_priority):
+    """This function uses both helper functions get_best_by_category
+    and swap_items to get the best item from self and vender, and
+    swap those items. True is returned"""
     best_self_item = self.get_best_by_category(their_priority)
     best_their_item = other.get_best_by_category(my_priority)
     best_item_swapped = self.swap_items(other, best_self_item, best_their_item)
     return best_item_swapped #is True
 
   def find_newest_item(self):
+    """This function is used to determine the newest_item by
+    comparing the ages. I want the item associated with shortest_age
+    to be the newest_item that is returned"""
     shortest_age = 100
     newest_item = None
     for item in self.inventory:
@@ -67,6 +91,9 @@ class Vendor():
     return newest_item
 
   def swap_by_newest(self, other):
+    """This function uses the helper function find_newest_item for
+    both self and other. Helper function swap_items is used to swap the
+    newest items, and returns True"""
     my_newest_item = self.find_newest_item() #age of 2.0 
     their_newest_item = other.find_newest_item() #age of 1.0
     swapped_transaction = self.swap_items(other, my_newest_item, their_newest_item)
