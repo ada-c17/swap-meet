6779c7c17d8ec08dc4aba49a9085cc6f0efb6e3a
Natalia <natalia.woodson@gmail.com>
2022-04-07 22:45:11 -0400

    Completed swap-meet

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,22 @@
 class Item:
-    pass
+    def __init__(self, category = "", condition=0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        condition_description = {
+            0: "Very Poor",
+            1: "Poor",
+            2: "Fair",
+            3: "Good",
+            4: "Excellent",
+            5: "Mint"   
+        }
+
+        condition = condition_description[self.condition]
+
+        return condition
+
