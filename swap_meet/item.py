a1023f30ec468000ecb22cefe8ac35baa6a2909a
Ruge Li <rugeli0605@gmail.com>
2022-04-07 21:34:44 -0700

    project completed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,30 @@
+#from swap_meet.vendor import Vendor
+
 class Item:
-    pass
+    def __init__(self,category="",condition=0,age=0):
+        self.category = category
+        self.condition = condition
+        self.age = age
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        condition = {
+            1:"poor",
+            2:"fair",
+            3:"good",
+            4:"like new",
+            5:"brand new"  
+        }
+        return f"condition is {condition[self.condition]}" 
+        
+
+        
+
+    
+        
+        
+
+        
+
+        
