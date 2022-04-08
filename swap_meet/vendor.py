7df84605863045e0232200944827d2aead2aa81b
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-04-07 17:20:19 -0700

    update comments and get_by_category_fuc

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,26 +12,22 @@ class Vendor:
         return item
 
     def remove(self,item):
-        # Use a try statement to check if the matching item can be removed from the inventory list
+        # Using a try statement to check if the matching item can be removed from the inventory list
         try:
             self.inventory.remove(item)
             return item
-        # Use except-clause to catch raised exception if there is no matching item in the inventory list 
+        # Using except-clause to catch raised exception if there is no matching item in the inventory list 
         except ValueError as err:
             return False
 
     def get_by_category(self, category):
-        # Create items list to store the item with matching category 
-        items = []
-        for item in self.inventory:
-            if item.category == category:
-                items.append(item)
-        return items 
+
+        return [item for item in self.inventory if item.category == category]
 
     def swap_items(self, friend, my_item, their_item):
-        # Check if self.inventory contains my_item and friend.inventory contains their_item
-        # Access the index of my_item and their_item in the inventory list 
-        # Swap the value of my_index and their_index (so that the time complexity is constant O(1))
+        # Checking if self.inventory contains my_item and friend.inventory contains their_item
+        # Accessing the index of my_item and their_item in the inventory list 
+        # Swaping the value of my_index and their_index (so that the time complexity is constant O(1))
         if my_item in self.inventory and their_item in friend.inventory:
             my_index = self.inventory.index(my_item)
             their_index = friend.inventory.index(their_item) 
@@ -48,34 +44,34 @@ class Vendor:
             return True
     
     def get_best_by_category(self, category):
-        # Initialize max_condition and max_value variables to capture the item with the highest condition and matching category
+        # Initializing max_condition and max_value variables to capture the item with the highest condition and matching category
         max_condition = 0
         max_value = None
 
         for item in self.inventory:
         # If the item matches the category and the item condition is greater than current max condition
-        # We will update the value of max condition and max_value
+        # The value of max condition and max_value will be updated
             if item.category == category and item.condition > max_condition:
                 max_condition = item.condition 
                 max_value = item
         return max_value 
 
     def swap_best_by_category(self, other, my_priority, their_priority):
-        # Call helper function get_best_by_category to return the best item avaiable that matches my_priority or their_priority category
-        # Call helper function swap_items to swap my_best and their_best items
+        # Calling helper function get_best_by_category to return the best item avaiable that matches my_priority or their_priority category
+        # Calling helper function swap_items to swap my_best_item and their_best_item
         if self.inventory and other.inventory:
-            my_best = self.get_best_by_category(their_priority)
-            their_best = other.get_best_by_category(my_priority)
+            my_best_item = self.get_best_by_category(their_priority)
+            their_best_item = other.get_best_by_category(my_priority)
 
-            return self.swap_items(other,my_best,their_best)
+            return self.swap_items(other,my_best_item,their_best_item)
     
     def swap_by_newest(self, other):
 
-        # Find the newest item in vendor's inventory list and other's inventory list
+        # Finding the newest item in vendor's inventory list and other's inventory list
         my_newest_item = min(self.inventory, key = attrgetter("age"))
         their_newest_item = min(other.inventory, key = attrgetter("age"))
         
-        # Call helper function swap_items to my_newest item and their_newest_item
+        # Call helper function swap_items to swap my_newest item and their_newest_item
         return self.swap_items(other,my_newest_item,their_newest_item)
             
 
