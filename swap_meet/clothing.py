854182afff5f071b9b825b77db9ed93ef54035cf
camilla122333 <pasadenastringtheory@gmail.com>
2022-04-07 22:21:53 -0700

    Add docstrings

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,12 @@
 from swap_meet.item import Item
+'''
+Clothing is a child class of Item. It is a type of Item (ClothingItem).
+Its attribute category has a namesake string value.
+'''
 
 class Clothing(Item):
-    def __init__(self, category = "Clothing", condition = 0):
-        super().__init__(category, condition) # new line
+    def __init__(self, condition = 0):
+        super().__init__(category = "Clothing", condition = condition)
 
     def __str__(self):
         return "The finest clothing you could wear."
