31230fbb74790efdbba362d99134f82687ad87fb
Sreymompech <sanchhen2013@gmail.com>
2022-04-07 19:39:03 -0700

    In Electronis class, re-setting 0 as default value of condition and age attributes in __init__()

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,14 +3,13 @@ class Electronics(Item):
     """Child class that inherits attributes and methods from Item class as parent class:"""
     
 
-    def __init__(self, condition = None, age = None):
+    def __init__(self, condition = 0, age = 0):
         """Setting category attribute as default with "Electronics" and inherits attributes from Item class."""
         super().__init__(category= "Electronics", condition = condition, age = age)
 
 
     def __str__(self):
         """Printing the returnning message string when printing instance of Electronics class."""
-        
         return "A gadget full of buttons and secrets."
 
     
