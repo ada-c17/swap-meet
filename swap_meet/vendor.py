b65ab02ed185ddc16cc690bd1492bebb871f31b6
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-04-07 20:00:50 -0400

    refactored Vendor class. All test still pass.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,12 +13,11 @@ class Vendor:
         return item
 
     def remove(self, item):
-        inventory_copy = [item for item in self.inventory]
-        if item in inventory_copy:
+        if item in self.inventory:
             self.inventory.remove(item)
             return item
-        else:
-            return False
+
+        return False
 
     def get_by_category(self, category):
         result = [item for item in self.inventory if item.category is category]
@@ -30,34 +29,36 @@ class Vendor:
         if not all_by_category:
             return None
 
-        max_rating = max({item.condition for item in all_by_category})
+        max_rating = max(item.condition for item in all_by_category)
         best_by_category = [
             item for item in all_by_category if item.condition == max_rating
         ]
-
+        # Assumed that in event of tie, return first item with highest rating
         return best_by_category[0]
 
     def swap_items(self, other_vendor, my_item, their_item):
-        if my_item in self.inventory and their_item in other_vendor.inventory:
-            self.remove(my_item)
-            self.add(their_item)
-            other_vendor.remove(their_item)
-            other_vendor.add(my_item)
-        else:
+        if not all([my_item in self.inventory, their_item in other_vendor.inventory]):
             return False
+
+        self.remove(my_item)
+        self.add(their_item)
+        other_vendor.remove(their_item)
+        other_vendor.add(my_item)
         return True
 
     def swap_best_by_category(self, other, my_priority, their_priority):
         item_they_want = self.get_best_by_category(their_priority)
         item_i_want = other.get_best_by_category(my_priority)
-        if item_they_want is None or item_i_want is None:
+
+        # Checks if either .get_best_by returned False
+        if not all([item_they_want, item_i_want]):
             return False
-        self.swap_items(other, item_they_want, item_i_want)
-        return True
+
+        return self.swap_items(other, item_they_want, item_i_want)
 
     def swap_first_item(self, other_vendor):
-        if not self.inventory or not other_vendor.inventory:
+        if not all([self.inventory, other_vendor.inventory]):
             return False
-        else:
-            self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
-            return True
+
+        self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
+        return True
