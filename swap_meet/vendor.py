ba1814ed8ed173774b0a0ff463389118ea7e4178
Victoria Shade <victoria.k.shade@gmail.com>
2022-04-08 06:19:07 -0700

    refactoring and docstrings added

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,21 @@
 class Vendor:
 
     def __init__(self, inventory = None):
+        '''Vendor class is instantiated'''
+
         if not inventory:
             inventory = []
         self.inventory = inventory
 
     def add(self, item):
+        '''Function used to add items to vendor's inventory'''
+
         self.inventory.append(item)
         return item
 
     def remove(self, item):
+        '''Function used to remove items from vendor's inventory'''
+
         if item not in self.inventory:
             return False
         else:
@@ -17,6 +23,8 @@ class Vendor:
             return item
 
     def get_by_category(self, category):
+        '''Function returns items in vendor's inventory if they belong to the category passed within the parameter'''
+
         items_in_category = []
         
         for item in self.inventory:
@@ -25,6 +33,8 @@ class Vendor:
         return items_in_category
 
     def swap_items(self, other, my_item, their_item):
+        '''Function allows this vendor and another vendor to swap items from their respective inventories'''
+
         if my_item not in self.inventory or their_item not in other.inventory:
             return False
         else:
@@ -36,57 +46,62 @@ class Vendor:
         return True
 
     def swap_first_item(self, other):
+        '''Function allows this vendor and another vendor to swap the first item from their respective inventories'''
+
         if not self.inventory or not other.inventory:
             return False
         else:
             return self.swap_items(other, self.inventory[0], other.inventory[0])
 
     def get_best_by_category(self, category):
+        '''Function identifies the item in the best condition within a certain category of a vendor's inventory'''
 
-        condition_of_items_in_category = []
+        current_condition = 0
+        best_condition = None
 
-        for item in self.get_by_category(category):
-                condition_of_items_in_category.append(item.condition)
+        for item in self.inventory:
+            if item.category == category:
+                if item.condition > current_condition:
+                    current_condition = item.condition
+                    best_condition = item
         
-        if not condition_of_items_in_category:
-            return None
-
-        best_condition = max(condition_of_items_in_category) #refactor with list comprehension?
-
-        if not best_condition:
-            return None
-        
-        for item in self.get_by_category(category):
-            if item.condition == best_condition:
-                return item
+        return best_condition
 
     def swap_best_by_category(self, other, my_priority, their_priority):
-        if not self.inventory or not other.inventory:
+        '''Function swaps the items in the best condition with that of other vendors if in a certain category'''
+
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+
+        if not my_best_item or not their_best_item:
             return False
-        
-        condition_my_best_item = []
+        else:
+            self.swap_items(other, my_best_item, their_best_item)
+            return True
+
+    def get_newest_by_category(self, category):
+        '''Function identifies the newest item within a certain category of a vendor's inventory'''
+
+        item_age_flag_max_30 = 30
+        newest_item = None
+
         for item in self.inventory:
-            condition_my_best_item.append(item.condition)
-
-        my_best_item = max(condition_my_best_item)
-
-        condition_their_best_item = []
-        for item in other.inventory:
-            condition_their_best_item.append(item.condition)
+            if item.category == category:
+                if item.age <= item_age_flag_max_30:
+                    item_age_flag_max_30 = item.age
+                    newest_item = item
         
-        other_vendor_best_item = max(condition_their_best_item)
+        return newest_item
 
-        my_trade_is_valid = False
-        for item in self.get_by_category(their_priority):
-            if item.condition == my_best_item:
-                my_item = item
-                my_trade_is_valid = True
+    def swap_newest_by_category(self, category, other):
+        '''Function swaps the newest item with that of other vendors newest item if in a certain category'''
 
-        their_trade_is_valid = False
-        for item in other.get_by_category(my_priority):
-            if item.condition == other_vendor_best_item:
-                their_item = item
-                their_trade_is_valid = True
+        my_newest_item = self.get_newest_by_category(category)
 
-        if my_trade_is_valid and their_trade_is_valid:
-            return self.swap_items(other, my_item, their_item)
+        their_newest_item = other.get_newest_by_category(category)
+
+        if not my_newest_item or not their_newest_item:
+            return False
+        else:
+            self.swap_items(other, my_newest_item, their_newest_item)
+            return True
