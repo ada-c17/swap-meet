982841ce8bfcccd758fa16f7e2f99dcf60e2b96f
Roshni Patel <rpatel01@wesleyan.edu>
2022-04-08 08:36:51 -0700

    Made note to self for question to ask about

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,6 +2,9 @@ from swap_meet.item import Item
 
 class Clothing(Item):
     def __init__(self, category = "Clothing", condition = 0, age = 1.0):
+        # Question - here in this super(),__init__, do I need to specify category is clothing e.g.
+        # category = "Clothing" OR because it's already stated in line 4 in the __init__ for Clothing,
+        # it doesn't need to be explicitly set again?
         super().__init__(category, condition, age)
 
     def __str__(self):
